759f1c900afc9713fe7952d1dd81425904cd7462
Johnny Benson <johnnybenson@users.noreply.github.com>
2021-04-26 20:51:53 -0400

    Restore Search Example

---
+++ how-to/2--add-searchable-content-in-home/src/browser.ts
@@ -0,0 +1,8 @@
+// The UUID that identifies the Home and Browser OpenFin platforn.
+export const browserPlatformUUID = "openfin-browser";
+
+// Full OpenFin identity of the Home and Browser OpenFin platforn.
+export const browserPlatformIdentity = {
+  uuid: browserPlatformUUID,
+  name: browserPlatformUUID,
+};
