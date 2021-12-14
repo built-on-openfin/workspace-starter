8b22a86c727b4f6e9cd4d78b7005283cdc34ee5a
John M <john.mandia@gmail.com>
2021-12-14 18:36:30 +0000

    Update list of how-tos to publish and improve manifest, json file url handling

--- scripts/package.js
+++ scripts/package.js
@@ -1,6 +1,8 @@
 const fs = require('fs-extra');
 const {execSync} = require('child_process')
 const package = require("../package.json");
+const replace = require('replace-in-file');
+
 
 fs.rmSync("public", { recursive: true, force: true });
 
@@ -15,4 +17,16 @@ for(let i = 0; i < package['how-tos'].length; i++) {
         stdio: 'inherit'
       });  
     fs.copySync('how-to/' + package['how-tos'][i] + '/public', 'public/' + package['how-tos'][i]);    
+    let options = {
+      files: 'public/' + package['how-tos'][i] + '/**/*.json',
+      from: /http:\/\/localhost:8080/g,
+      to: 'https://built-on-openfin.github.io/workspace-starter/v' + package.version + '/' + package['how-tos'][i],
+    };
+    try {
+      let results = replace.sync(options);
+      console.log('Replacement results:', results);
+    }
+    catch (error) {
+      console.error('Error occurred:', error);
+    }
 }
