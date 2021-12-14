c6dd0f21fe524063203e7e563b894a32d7bc2712
John M <john.mandia@gmail.com>
2021-12-14 14:51:18 +0000

    Preparation for github actions

---
+++ scripts/package.js
@@ -0,0 +1,18 @@
+const fs = require('fs-extra');
+const {execSync} = require('child_process')
+const package = require("../package.json");
+
+fs.rmSync("public/" + package.version, { recursive: true, force: true });
+
+for(let i = 0; i < package['how-tos'].length; i++) {
+    let howto =  package['how-tos'][i];
+    execSync('npm install', {
+        cwd: 'how-to/' + howto,
+        stdio: 'inherit'
+      });
+    execSync('npm run build-client', {
+        cwd: 'how-to/' + howto,
+        stdio: 'inherit'
+      });  
+    fs.copySync('how-to/' + package['how-tos'][i] + '/public', 'public/' + package.version + '/' + package['how-tos'][i]);    
+}
