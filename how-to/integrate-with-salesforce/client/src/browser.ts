4143f592cbbac4cd24e60e8fe0222c108b07201e
Rich Browne <richard.browne@openfin.co>
2021-12-21 21:13:13 +0000

    Browser window now opens at a better size for viewing Salesforce content.

--- how-to/integrate-with-salesforce/client/src/browser.ts
+++ how-to/integrate-with-salesforce/client/src/browser.ts
@@ -1,7 +1,29 @@
 import { getCurrentSync } from '@openfin/workspace-platform';
 
+let windowIdentity: OpenFin.Identity;
+
 export async function launchView(url: string) {
     const platform = getCurrentSync();
-    const windowIdentity = await platform.Browser.getLastFocusedWindow();
+    let createWindow = true;
+    if (windowIdentity) {
+        const window = fin.Window.wrapSync(windowIdentity);
+        if (window) {
+            createWindow = false;
+        }
+    }
+    if (createWindow) {
+        windowIdentity = await platform.createWindow({
+            defaultHeight: 700,
+            defaultWidth: 1200,
+            layout: {
+              content: [
+                {
+                  type: 'stack',
+                  content: [],
+                },
+              ],
+            },
+          });
+    }
     await platform.createView({ url, target: null }, windowIdentity);
 }
