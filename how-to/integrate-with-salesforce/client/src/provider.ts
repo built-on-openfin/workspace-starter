27362da5e4047775167fcfc534e9427807e19a1e
Rich Browne <richard.browne@openfin.co>
2021-12-22 22:57:56 +0000

    Updated provider module to display an error popup if platform initialization fails. Added popup assets.

--- how-to/integrate-with-salesforce/client/src/provider.ts
+++ how-to/integrate-with-salesforce/client/src/provider.ts
@@ -4,7 +4,31 @@ import { init as bootstrap } from './bootstrapper';
 import { fin } from 'openfin-adapter/src/mock';
 
 window.addEventListener('DOMContentLoaded', async () => {
-  let platform = fin.Platform.getCurrentSync();
+  const platform = fin.Platform.getCurrentSync();
   platform.once('platform-api-ready', bootstrap.bind(this));
-  await initialisePlatform();
+  try {
+    await initialisePlatform();
+  } catch (err) {
+    const title = 'Failed to start Workspace platform';
+    const body = `<p>An error has occured during Workspace platform initialization:</p><p class="error">${(err as Error).message}.</p><p>Please check your configuration and try again.</p>`;
+    const window = await fin.Window.create({
+      alwaysOnTop: true,
+      maximizable: false,
+      minimizable: false,
+      autoShow: true,
+      defaultCentered: true,
+      defaultHeight: 250,
+      defaultWidth: 500,
+      includeInSnapshots: false,
+      name: 'popup',
+      resizable: false,
+      saveWindowState: false,
+      shadow: true,
+      smallWindow: true,
+      showTaskbarIcon: false,
+      url: `/popup.html?title=${encodeURIComponent(title)}&body=${encodeURIComponent(body)}`,
+    });
+    window.addListener('closed', () => platform.Application.close(true));
+    await window.focus();
+  }
 });
