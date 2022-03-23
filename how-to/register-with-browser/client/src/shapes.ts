c86b65b73315bfdc73bda0b8c40629983726187a
adam-saland <51667958+adam-saland@users.noreply.github.com>
2022-03-23 10:21:57 -0400

    Adam/how to/register with browser (#63)
    Example of using a custom UI to launch customized browser pages.

---
+++ how-to/register-with-browser/client/src/shapes.ts
@@ -0,0 +1,24 @@
+import { CustomThemes } from "@openfin/workspace-platform";
+
+interface BrowserProvider {
+    windowOptions: {
+        title?:string,
+        icon?:string,
+        newTabUrl?: string;
+        newPageUrl?: string;
+    }
+}
+
+interface ThemeProvider {
+    themes: CustomThemes
+}
+
+interface LaunchBarWindowSettings {
+    url: string,
+    options: object
+}
+export interface CustomSettings {
+    bootstrap?: { launchBarWindowSettings?: LaunchBarWindowSettings }
+    browserProvider?: BrowserProvider,
+    themeProvider?: ThemeProvider
+}
