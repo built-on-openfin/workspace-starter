04a7dd7e189ed712ffa40a7226e2da29062f3c6a
John M <john.mandia@gmail.com>
2022-04-11 22:03:40 +0100

    First version of script to support mac and window launches.

---
+++ how-to/customize-workspace/scripts/launch.mjs
@@ -0,0 +1,58 @@
+import { launch, connect } from "openfin-adapter";
+
+async function launchFromNode(manifestUrl) {
+  try {
+    console.log("Launching manifest: " + manifestUrl);
+
+    const port = await launch({ manifestUrl });
+
+    //We will use the port to connect from Node to determine when OpenFin exists.
+    const fin = await connect({
+      uuid: "dev-connection-" + Date.now(), //Supply an addressable Id for the connection
+      address: `ws://localhost:${port}`, //Connect to the given port.
+      nonPersistent: true, //We want OpenFin to exit as our application exists.
+    });
+
+    //Once OpenFin exists we shut down the process.
+    fin.once("disconnected", process.exit);
+    return fin;
+  } catch (e) {
+    throw new Error(`Error: \n${e}`);
+  }
+}
+
+(async () => {
+  try {
+    const launchArgs = process.argv.slice(2);
+
+    let manifestUrl = "http://localhost:8080/manifest.fin.json";
+
+    if (launchArgs.length > 0) {
+      manifestUrl = launchArgs[0];
+    }
+
+    const fin = await launchFromNode(manifestUrl, launch, connect);
+
+    const manifest = await fin.System.fetchManifest(manifestUrl);
+    const platform = fin.Platform.wrapSync({ uuid: manifest.platform.uuid });
+    console.log("Wrapped target platform: " + manifest.platform.uuid);
+    // //do something when app is closing
+    process.on("exit", async () => {
+      console.log("Exit called");
+      await platform.quit();
+    });
+
+    // //catches ctrl+c event
+    process.on("SIGINT", async () => {
+      console.log("Ctrl + C called");
+      await platform.quit();
+      process.exit();
+    });
+
+    console.log(
+      `You successfully connected from node with to the manifest: ${manifestUrl}`
+    );
+  } catch (e) {
+    throw new Error(`Error connecting to  from node: \n${e}`);
+  }
+})();
