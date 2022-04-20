2b535578bc458233a67bf4b851de412e835b31ff
Martyn Janes <martyn@obany.com>
2022-04-20 14:51:01 +0100

    Remove unneccesary build output

--- how-to/customize-workspace/packages/workspace-integration/es/shapes.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2hhcGVzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL3NoYXBlcy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIn0=
