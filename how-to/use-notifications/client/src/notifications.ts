d15a12d4e49607521ac5c27080f47944a103aa32
Martyn Janes <martyn@obany.com>
2022-05-09 13:37:09 +0100

    Add actionable and form notifications

--- how-to/use-notifications/client/src/notifications.ts
+++ how-to/use-notifications/client/src/notifications.ts
@@ -1,4 +1,3 @@
-import { getCurrentSync } from '@openfin/workspace-platform';
 import {
     addEventListener as addNotificationEventListener,
     create,
@@ -30,8 +29,14 @@ async function initDom() {
     const btnNotificationSimple = document.querySelector("#btnNotificationSimple");
     btnNotificationSimple.addEventListener("click", async () => showSimpleNotification())
 
-    const btnNotificationCenterToggle = document.querySelector("#btnNotificationCenterToggle");
-    btnNotificationCenterToggle.addEventListener("click", async () => toggleNotificationCenter());
+    const btnNotificationActionable = document.querySelector("#btnNotificationActionable");
+    btnNotificationActionable.addEventListener("click", async () => showActionableNotification())
+
+    const btnNotificationForm = document.querySelector("#btnNotificationForm");
+    btnNotificationForm.addEventListener("click", async () => showFormNotification())
+
+    const btnNotificationsCenterToggle = document.querySelector("#btnNotificationsCenterToggle");
+    btnNotificationsCenterToggle.addEventListener("click", async () => toggleNotificationCenter());
 
     const btnNotificationStudioOpen: HTMLButtonElement = document.querySelector("#btnNotificationStudioOpen");
     btnNotificationStudioOpen.addEventListener("click", async () => {
@@ -48,19 +53,24 @@ async function initDom() {
 
 function initListener() {
     addNotificationEventListener("notification-created", event => {
-        loggingAddEntry(`Created ${event.notification.id}`);
+        loggingAddEntry(`Created: ${event.notification.id}`);
     });
 
     addNotificationEventListener("notification-closed", event => {
-        loggingAddEntry(`Closed ${event.notification.id}`);
+        loggingAddEntry(`Closed: ${event.notification.id}`);
     });
 
     addNotificationEventListener("notification-action", event => {
-        loggingAddEntry(`Action ${event.notification.id}`);
+        loggingAddEntry(`\tData: ${event?.result?.customData ? JSON.stringify(event.result.customData) : "None"}`);
+        loggingAddEntry(`\tTask: ${event?.result?.task ?? "None"}`);
+        loggingAddEntry(`Action: ${event.notification.id}`);
+        console.log(event);
     });
 
     addNotificationEventListener("notification-form-submitted", event => {
-        loggingAddEntry(`Action ${event.notification.id}`);
+        loggingAddEntry(`\tData: ${event?.form ? JSON.stringify(event.form) : "None"}`);
+        loggingAddEntry(`Form: ${event.notification.id}`);
+        console.log(event)
     });
 
     addNotificationEventListener("notifications-count-changed", event => {
@@ -74,13 +84,13 @@ function loggingShowHide(): void {
 }
 
 function loggingAddEntry(entry: string): void {
-    loggingElement.innerText = `${entry}\n\n` + loggingElement.innerText;
+    loggingElement.innerHTML = `${entry}\n\n` + loggingElement.innerHTML;
     loggingShowHide();
 }
 
 function showNotificationCount(count: number): void {
-    const notificationCountElement: HTMLElement = document.querySelector("#notificationCount");
-    notificationCountElement.innerText = `Notification Count: ${count}`;
+    const btnNotificationsCenterToggle: HTMLElement = document.querySelector("#btnNotificationsCenterToggle");
+    btnNotificationsCenterToggle.innerText = `Notifications Center [${count}]`;
 }
 
 async function showSimpleNotification() {
@@ -91,6 +101,86 @@ async function showSimpleNotification() {
         category: "default",
         template: "markdown",
         id: crypto.randomUUID()
+    }
+
+    await create(notification);
+}
+
+async function showActionableNotification() {
+    const notification: NotificationOptions = {
+        title: "Actionable Notification",
+        body: "This is a notification that has an action",
+        toast: "transient",
+        category: "default",
+        template: "markdown",
+        id: crypto.randomUUID(),
+        buttons: [
+            {
+                title: "Acknowledged",
+                type: "button",
+                cta: true,
+                onClick: {
+                    task: "acknowledge-task",
+                    customData: {
+                        message: "This is the response data"
+                    }
+                }
+            },
+            {
+                title: "Cancel",
+                type: "button"
+            }
+        ]
+    }
+
+    await create(notification);
+}
+
+async function showFormNotification() {
+    const notification: NotificationOptions = {
+        title: "Form Notification",
+        body: "This is a notification that has form data",
+        toast: "transient",
+        category: "default",
+        template: "markdown",
+        id: crypto.randomUUID(),
+        form: [
+            {
+                key: 'amount',
+                label: 'Amount',
+                type: 'number',
+                widget: {
+                    type: 'Number',
+                    max: 100,
+                    min: 1
+                },
+                validation: {
+                    min: {
+                        arg: 1,
+                        invalidMessage: 'Must be at least 1'
+                    },
+                    max: {
+                        arg: 100,
+                        invalidMessage: 'Cannot be more than 100'
+                    },
+                    required: {
+                        arg: true
+                    }
+                }
+            }
+        ],
+        buttons: [
+            {
+                title: "Save",
+                type: "button",
+                cta: true,
+                submit: true
+            },
+            {
+                title: "Cancel",
+                type: "button"
+            }
+        ]
     }
 
     await create(notification);
