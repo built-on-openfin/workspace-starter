dc8e7ba39f13b4fa8e21ce8105839d2c93eba23d
John M <john.mandia@gmail.com>
2022-05-25 12:11:42 +0100

    update after feedback

--- how-to/customize-workspace/client/src/settings.ts
+++ how-to/customize-workspace/client/src/settings.ts
@@ -25,12 +25,8 @@ export async function getSettings(): Promise<CustomSettings> {
 }
 
 function getSystemPreferredColorScheme() {
-    if (window.matchMedia) {
-      if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         return 'dark';
-      } else {
-        return 'light';
-      }
     }
     return 'light';
   }
