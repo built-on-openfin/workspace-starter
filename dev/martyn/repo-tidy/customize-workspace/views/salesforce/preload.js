9a00e132188fcd291cebf1cc2e6d3bf1166388d8
John M <john.mandia@gmail.com>
2022-06-09 16:40:35 +0100

    Cleaning up gh-pages
    gh-pages has got quite large and these branches do not need to be present unless they are testing latest changes. Other than the main branches and /workspace and the legacy how-to folder.

--- dev/martyn/repo-tidy/customize-workspace/views/salesforce/preload.js
+++
@@ -1,4 +0,0 @@
-(() => {
-  'use strict';
-  void 0 !== window.fin && ((CustomEvent.prototype.fin = window.fin), (CustomEvent.prototype.fdc3 = window.fdc3));
-})();
--- headless-app/register-with-home/views/preload.js
+++
@@ -1,4 +0,0 @@
-
-console.log("Hello World");
-debugger;
-console.log("Hello World");
