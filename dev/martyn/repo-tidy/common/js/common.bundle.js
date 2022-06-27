6d245e8b2e5e6c63dab39894760373501d38fc81
obany <martyn@obany.com>
2022-06-27 10:22:53 +0000

    Deploying to gh-pages from @ built-on-openfin/workspace-starter@80ce93b0b167c00fa0412a30c1435e6960f59f2f 🚀

---
+++ dev/martyn/repo-tidy/common/js/common.bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*****************************!*\
+  !*** ./client/src/index.ts ***!
+  \*****************************/
+console.log("Placeholder. No common scripts just yet.");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29tbW9uLmJ1bmRsZS5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLE9BQU8sQ0FBQyxHQUFHLENBQUMsMENBQTBDLENBQUMsQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovL29wZW5maW4td29ya3NwYWNlLS1jb21tb24vLi9jbGllbnQvc3JjL2luZGV4LnRzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwiUGxhY2Vob2xkZXIuIE5vIGNvbW1vbiBzY3JpcHRzIGp1c3QgeWV0LlwiKTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
