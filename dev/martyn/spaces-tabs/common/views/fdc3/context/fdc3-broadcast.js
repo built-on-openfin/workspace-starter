0cf3b9c3bed8a8887890969b3a0ee927c0c4d1ff
obany <martyn@obany.com>
2022-06-28 14:57:46 +0000

    Deploying to gh-pages from @ built-on-openfin/workspace-starter@43ce6f6e3531e5b3cd723426185a9d98ee5e34c9 🚀

---
+++ dev/martyn/spaces-tabs/common/views/fdc3/context/fdc3-broadcast.js
@@ -0,0 +1,49 @@
+// -------------------------------------------------
+// FDC3 Functions
+// -------------------------------------------------
+export async function systemBroadcast(log, context) {
+	if (window.fdc3 !== undefined) {
+		const systemChannel = await fdc3.getCurrentChannel();
+		if (systemChannel !== null) {
+			log(`broadcasting on ${systemChannel.type} channel: ${systemChannel.id}`, context);
+			fdc3.broadcast(context);
+		} else {
+			log('You are not bound to a system channel and are unable to broadcast:', context);
+		}
+	}
+}
+
+export async function appBroadcast(log, appChannelName, context) {
+	if (window.fdc3 !== undefined && appChannelName !== undefined) {
+		const appChannel = await fdc3.getOrCreateChannel(appChannelName);
+		log(`broadcasting on ${appChannel.type} channel: ${appChannel.id}`, context);
+		appChannel.broadcast(context);
+	}
+}
+
+export async function listenToSystemBroadcast(log, onContextReceived, ctxType = null) {
+	if (window.fdc3 !== undefined) {
+		const systemHandler = (ctx) => {
+			log('System Context Received: ', ctx);
+			onContextReceived();
+		};
+
+		log('Listening for system context.');
+		fdc3.addContextListener(ctxType, systemHandler);
+	}
+}
+
+export async function listenToAppBroadcast(log, appChannelName, onContextReceived) {
+	if (window.fdc3 !== undefined && appChannelName !== undefined) {
+		// listen to a defined application channel
+		const appHandler = (ctx) => {
+			log('App Channel Context Received: ', ctx);
+			onContextReceived();
+		};
+		await fdc3.getOrCreateChannel(appChannelName);
+
+		// listen for new app channel messages
+		log(`Listening for app channel: ${appChannelName} context.`);
+		appChannel.addContextListener(null, appHandler);
+	}
+}
