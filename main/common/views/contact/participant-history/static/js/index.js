a82299387eb8d60d8142b57a9cfb35ebe6d480af
obany <martyn@obany.com>
2022-06-28 16:32:25 +0000

    Deploying to gh-pages from @ built-on-openfin/workspace-starter@f256b75c1e3fa7364ff3e5611451fdc66029dd24 🚀

--- main/common/views/contact/participant-history/static/js/index.js
+++ main/common/views/contact/participant-history/static/js/index.js
@@ -1,28 +1,28 @@
 function init() {
-  if (window.fdc3) {
-    const contextHandler = (ctx) => {
-      console.log('Context Received:', ctx);
-      if (ctx.type === 'fdc3.contact') {
-        setContact(ctx);
-      }
-    };
+	if (window.fdc3) {
+		const contextHandler = (ctx) => {
+			console.log('Context Received:', ctx);
+			if (ctx.type === 'fdc3.contact') {
+				setContact(ctx);
+			}
+		};
 
-    window.fdc3.addContextListener(contextHandler);
+		window.fdc3.addContextListener(contextHandler);
 
-    window.fdc3.addIntentListener('ViewContact', contextHandler);
-  }
+		window.fdc3.addIntentListener('ViewContact', contextHandler);
+	}
 }
 
 function setContact(ctx) {
-  const randomId = (Math.random() * 1000000).toFixed(0);
+	const randomId = (Math.random() * 1000000).toFixed(0);
 
-  const userNameContainers = document.querySelectorAll('.username');
+	const userNameContainers = document.querySelectorAll('.username');
 
-  const username = `${ctx.name} (${randomId})`;
+	const username = `${ctx.name} (${randomId})`;
 
-  for (let i = 0; i < userNameContainers.length; i++) {
-    userNameContainers[i].textContent = username;
-  }
+	for (let i = 0; i < userNameContainers.length; i++) {
+		userNameContainers[i].textContent = username;
+	}
 }
 
 window.addEventListener('DOMContentLoaded', init);
