9d9ec447b778909f9c75b0eb8b4b07ddc043cd1c
obany <martyn@obany.com>
2022-06-28 10:39:58 +0000

    Deploying to gh-pages from @ built-on-openfin/workspace-starter@baa6c4482473fc76b31e8693b953f8d81ff7a06e 🚀

--- main/common/views/interop/context/interop-broadcast.js
+++ main/common/views/interop/context/interop-broadcast.js
@@ -4,59 +4,46 @@
 export async function systemSetContext(log, context) {
   if (window.fin !== undefined) {
     try {
-      log(
-        `set context being called on system channel:`,
-        context
-      );
+      log(`set context being called on system channel:`, context);
       fin.me.interop.setContext(context);
-    } catch(error) {
-      log(
-        "You are not bound to a system channel and are unable to set context:",
-        error
-      );
+    } catch (error) {
+      log('You are not bound to a system channel and are unable to set context:', error);
     }
   }
 }
 
 export async function sessionSetContext(log, appSessionContextGroupName, context) {
-  if (window.fin !== undefined) {
-    if (appSessionContextGroupName !== undefined) {
-      const appSessionContextGroup = await fin.me.interop.joinSessionContextGroup(appSessionContextGroupName);
-      log(
-        `Setting context on session context group ${appSessionContextGroupName}`,
-        context
-      );
-      appSessionContextGroup.setContext(context);
-    }
+  if (window.fin !== undefined && appSessionContextGroupName !== undefined) {
+    const appSessionContextGroup = await fin.me.interop.joinSessionContextGroup(appSessionContextGroupName);
+    log(`Setting context on session context group ${appSessionContextGroupName}`, context);
+    appSessionContextGroup.setContext(context);
   }
 }
 
 export async function listenToSystemContext(log, onContextReceived) {
   if (window.fin !== undefined) {
     const systemHandler = (ctx) => {
-      log("System Context Received: ", ctx);
+      log('System Context Received: ', ctx);
       onContextReceived();
     };
 
-    log("Listening for system context.");
-    const systemListener = fin.me.interop.addContextHandler(systemHandler);
+    log('Listening for system context.');
+    fin.me.interop.addContextHandler(systemHandler);
   }
 }
 
 export async function listenToSessionContext(log, appSessionContextGroupName, onContextReceived) {
-  if (window.fin !== undefined) {
-    if (appSessionContextGroupName !== undefined) {
-      // listen to a defined session context group
-      const appHandler = (ctx) => {
-        log("App Session Context Received: ", ctx);
-        onContextReceived();
-      };
+  if (window.fin !== undefined && appSessionContextGroupName !== undefined) {
+    // listen to a defined session context group
+    const appHandler = (ctx) => {
+      log('App Session Context Received: ', ctx);
+      onContextReceived();
+    };
 
-      const appSessionContextGroup = await fin.me.interop.joinSessionContextGroup(appSessionContextGroupName);
-     
-       // listen for new app channel messages
-      log("Listening for app session context group: " + appSessionContextGroupName + " context.");
-      let appListener = appSessionContextGroup.addContextHandler(appHandler);
-    }
+    await fin.me.interop.joinSessionContextGroup(appSessionContextGroupName);
+
+    // listen for new app channel messages
+    log(`Listening for app session context group: ${appSessionContextGroupName} context.`);
+    appSessionContextGroup.addContextHandler(appHandler);
   }
 }
