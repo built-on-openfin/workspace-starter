66e62b553cc98838fbbcd01d29eb5de49881568e
John <john.mandia@gmail.com>
2022-07-04 16:24:59 +0100

    Add additional manifestType examples (#146)
    * Add additional manifestType examples
    
    Add additional examples to show:
    
    * How you can launch a headless window to do background processing
    * How you can launch a classic window from home (inline definition or via a manifest json file)
    * How you can launch the desktop browser if needed (maybe a site that is only compatible with an older desktop browser).
    
    You need to run npm run setup to update and build the common folder (which has the hidden window code).
    
    The only sample that demonstrates this is the customize-workspace sample as it is giving an example of launching custom manifest types. New manifest types:
    
    * window
    * inline-window
    * desktop-browser
    
    The manifests in customize-workspace have been updated to opt into displaying these manifest types.
    
    * Update to switch statement for resolving label and action text

--- how-to/common/client/webpack.config.js
+++ how-to/common/client/webpack.config.js
@@ -1,22 +1,31 @@
 const path = require('path');
 
-module.exports = {
-	entry: './client/src/index.ts',
-	devtool: 'inline-source-map',
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js']
-	},
-	output: {
-		filename: 'common.bundle.js',
-		path: path.resolve(__dirname, '..', 'public', 'js')
+module.exports = [
+	{
+		entry: './client/src/windows/hidden-window/hidden.ts',
+		devtool: 'inline-source-map',
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					use: 'ts-loader',
+					exclude: /node_modules/
+				}
+			]
+		},
+		resolve: {
+			extensions: ['.tsx', '.ts', '.js']
+		},
+		externals: { fin: 'fin' },
+		output: {
+			filename: 'common.windows.hidden.bundle.js',
+			library: {
+				type: 'module'
+			},
+			path: path.resolve(__dirname, '..', 'public', 'js')
+		},
+		experiments: {
+			outputModule: true
+		}
 	}
-};
+];
