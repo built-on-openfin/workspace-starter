d8f44381c20fa9420374482cfdb12d7b165e0374
John M <john.mandia@gmail.com>
2022-07-08 19:17:30 +0100

    Updated to support an intent for create an app definition
    Updated to support a right click action on a view to create an app definition by raising an intent.
    Made the view hosting the new tab fdc3 compatible in case you wanted to navigate to a page that should support fdc3.
    Made action ids come from a central place and used them in actions.ts and menu.ts.
    Updated config support so you can decide from a manifest which custom menu actions you wish to support.
    Added more of the manager portal views to app.json add added intent support to the app config builder.

--- how-to/customize-workspace/client/src/actions.ts
+++ how-to/customize-workspace/client/src/actions.ts
@@ -1,6 +1,7 @@
 import {
 	BrowserCreateWindowRequest,
 	CustomActionCallerType,
+	CustomActionPayload,
 	CustomActionsMap,
 	getCurrentSync
 } from "@openfin/workspace-platform";
@@ -26,139 +27,216 @@ async function getViewWindowIdentity(view: OpenFin.View): Promise<OpenFin.Identi
 	return currentWindow.identity;
 }
 
+export const ACTION_IDS = {
+	raiseCreateAppDefinitionIntent: "raise-create-app-definition-intent",
+	moveViewToNewWindow: "move-view-to-new-window",
+	movePageToNewWindow: "move-page-to-new-window",
+	pinWindow: "pin-window",
+	unpinWindow: "unpin-window",
+	homeShow: "home-show",
+	notificationToggle: "notification-toggle",
+	share: "share",
+	changeOpacity: "change-opacity",
+	restoreOpacity: "restore-opacity"
+};
+
 export async function getActions(): Promise<CustomActionsMap> {
-	return {
-		"move-view-to-new-window": async (payload) => {
-			if (payload.callerType === CustomActionCallerType.ViewTabContextMenu) {
-				const platform = getCurrentSync();
-				const initialView = await platform.createView({
-					name: payload.selectedViews[0].name
-				} as OpenFin.PlatformViewCreationOptions);
-				if (payload.selectedViews.length > 1) {
-					const windowIdentity = await getViewWindowIdentity(initialView);
-					for (let i = 1; i < payload.selectedViews.length; i++) {
-						await platform.createView(
-							{
-								name: payload.selectedViews[i].name
-							} as OpenFin.PlatformViewCreationOptions,
-							windowIdentity,
-							initialView.identity
-						);
+	const actionMap: CustomActionsMap = {};
+	actionMap[ACTION_IDS.raiseCreateAppDefinitionIntent] = async (payload: CustomActionPayload) => {
+		if (payload.callerType === CustomActionCallerType.ViewTabContextMenu) {
+			const brokerClient = fin.Interop.connectSync(fin.me.identity.uuid, {});
+			for (let i = 0; i < payload.selectedViews.length; i++) {
+				const viewIdentity = payload.selectedViews[i];
+				const intentName = "CreateAppDefinition";
+				try {
+					const view = fin.View.wrapSync(viewIdentity);
+					const options = await view.getOptions();
+					const info = await view.getInfo();
+					const name = options.name;
+					const preloads =
+						Array.isArray(options.preloadScripts) && options.preloadScripts.length > 0
+							? options.preloadScripts
+							: undefined;
+					const manifest = {
+						url: info.url,
+						fdc3InteropApi: options.fdc3InteropApi,
+						interop: options.interop,
+						customData: options.customData,
+						preloadScripts: preloads
+					};
+					const icons = [];
+					const favicons = info.favicons || [];
+					for (let f = 0; f < favicons.length; f++) {
+						icons.push({ src: favicons[f] });
 					}
-				}
-			}
-		},
-		"move-page-to-new-window": async (payload) => {
-			if (payload.callerType === CustomActionCallerType.PageTabContextMenu) {
-				const platform = getCurrentSync();
-				const windowOptions = await getDefaultWindowOptions();
-				const win = platform.Browser.wrapSync(payload.windowIdentity);
-				const page = await win.getPage(payload.pageId);
-				windowOptions.workspacePlatform.pages = [page];
-				await platform.createWindow(windowOptions);
-				await win.removePage(page.pageId);
-			}
-		},
-		"pin-window": async (payload) => {
-			if (payload.callerType === CustomActionCallerType.CustomButton) {
-				const platform = getCurrentSync();
-				const browserWindow = platform.Browser.wrapSync(payload.windowIdentity);
-				const options = await browserWindow.openfinWindow.getOptions();
-				const currentToolbarOptions = (options as BrowserCreateWindowRequest).workspacePlatform
-					.toolbarOptions;
-				await browserWindow.openfinWindow.updateOptions({ alwaysOnTop: true });
-				if (currentToolbarOptions !== undefined && currentToolbarOptions !== null) {
-					const newButtons = await updateToolbarButtons(
-						currentToolbarOptions.buttons,
-						payload.customData.sourceId as string,
-						payload.customData.replacementId as string
+					const app = {
+						appId: name,
+						name,
+						title: info.title,
+						description: info.title,
+						manifestType: "inline-view",
+						manifest,
+						tags: ["view"],
+						icons,
+						images: [],
+						publisher: "",
+						contactEmail: "",
+						supportEmail: "",
+						intents: []
+					};
+					const intent = {
+						name: intentName,
+						context: {
+							type: "openfin.app",
+							app
+						}
+					};
+					await brokerClient.fireIntent(intent);
+				} catch (error) {
+					console.error(
+						`Error while trying to raise intent ${intentName} for view ${viewIdentity.name}`,
+						error
 					);
-					await browserWindow.replaceToolbarOptions({ buttons: newButtons });
-				}
-			}
-
-			if (payload.callerType === CustomActionCallerType.ViewTabContextMenu) {
-				const platform = getCurrentSync();
-				const browserWindow = platform.Browser.wrapSync(payload.windowIdentity as OpenFin.Identity);
-				const options = await browserWindow.openfinWindow.getOptions();
-				const currentToolbarOptions = (options as BrowserCreateWindowRequest).workspacePlatform
-					.toolbarOptions;
-				await browserWindow.openfinWindow.updateOptions({ alwaysOnTop: true });
-				if (currentToolbarOptions !== undefined && currentToolbarOptions !== null) {
-					const newButtons = await updateToolbarButtons(
-						currentToolbarOptions.buttons,
-						payload.customData.sourceId as string,
-						payload.customData.replacementId as string
-					);
-					await browserWindow.replaceToolbarOptions({ buttons: newButtons });
-				}
-			}
-		},
-		"unpin-window": async (payload) => {
-			if (payload.callerType === CustomActionCallerType.CustomButton) {
-				const platform = getCurrentSync();
-				const browserWindow = platform.Browser.wrapSync(payload.windowIdentity);
-				const options = await browserWindow.openfinWindow.getOptions();
-				const currentToolbarOptions = (options as BrowserCreateWindowRequest).workspacePlatform
-					.toolbarOptions;
-				await browserWindow.openfinWindow.updateOptions({ alwaysOnTop: false });
-				if (currentToolbarOptions !== undefined && currentToolbarOptions !== null) {
-					const newButtons = await updateToolbarButtons(
-						currentToolbarOptions.buttons,
-						payload.customData.sourceId as string,
-						payload.customData.replacementId as string
-					);
-					await browserWindow.replaceToolbarOptions({ buttons: newButtons });
-				}
-			}
-		},
-		"home-show": async () => {
-			await show();
-		},
-		"notification-toggle": async () => {
-			await toggleNotificationCenter();
-		},
-		share: async (payload) => {
-			if (payload.callerType === CustomActionCallerType.CustomButton) {
-				await showShareOptions(payload);
-			}
-		},
-		"change-opacity": async (payload) => {
-			if (payload.callerType === CustomActionCallerType.CustomButton) {
-				const platform = getCurrentSync();
-				const browserWindow = platform.Browser.wrapSync(payload.windowIdentity);
-				const options = await browserWindow.openfinWindow.getOptions();
-				const currentToolbarOptions = (options as BrowserCreateWindowRequest).workspacePlatform
-					.toolbarOptions;
-				await browserWindow.openfinWindow.updateOptions({ opacity: 0.7 });
-				if (currentToolbarOptions !== undefined && currentToolbarOptions !== null) {
-					const newButtons = await updateToolbarButtons(
-						currentToolbarOptions.buttons,
-						payload.customData.sourceId as string,
-						payload.customData.replacementId as string
-					);
-					await browserWindow.replaceToolbarOptions({ buttons: newButtons });
-				}
-			}
-		},
-		"restore-opacity": async (payload) => {
-			if (payload.callerType === CustomActionCallerType.CustomButton) {
-				const platform = getCurrentSync();
-				const browserWindow = platform.Browser.wrapSync(payload.windowIdentity);
-				const options = await browserWindow.openfinWindow.getOptions();
-				const currentToolbarOptions = (options as BrowserCreateWindowRequest).workspacePlatform
-					.toolbarOptions;
-				await browserWindow.openfinWindow.updateOptions({ opacity: 1 });
-
-				if (currentToolbarOptions !== undefined && currentToolbarOptions !== null) {
-					const newButtons = await updateToolbarButtons(
-						currentToolbarOptions.buttons,
-						payload.customData.sourceId as string,
-						payload.customData.replacementId as string
-					);
-					await browserWindow.replaceToolbarOptions({ buttons: newButtons });
 				}
 			}
 		}
 	};
+
+	actionMap[ACTION_IDS.moveViewToNewWindow] = async (payload: CustomActionPayload) => {
+		if (payload.callerType === CustomActionCallerType.ViewTabContextMenu) {
+			const platform = getCurrentSync();
+			const initialView = await platform.createView({
+				name: payload.selectedViews[0].name
+			} as OpenFin.PlatformViewCreationOptions);
+			if (payload.selectedViews.length > 1) {
+				const windowIdentity = await getViewWindowIdentity(initialView);
+				for (let i = 1; i < payload.selectedViews.length; i++) {
+					await platform.createView(
+						{
+							name: payload.selectedViews[i].name
+						} as OpenFin.PlatformViewCreationOptions,
+						windowIdentity,
+						initialView.identity
+					);
+				}
+			}
+		}
+	};
+
+	actionMap[ACTION_IDS.movePageToNewWindow] = async (payload: CustomActionPayload) => {
+		if (payload.callerType === CustomActionCallerType.PageTabContextMenu) {
+			const platform = getCurrentSync();
+			const windowOptions = await getDefaultWindowOptions();
+			const win = platform.Browser.wrapSync(payload.windowIdentity);
+			const page = await win.getPage(payload.pageId);
+			windowOptions.workspacePlatform.pages = [page];
+			await platform.createWindow(windowOptions);
+			await win.removePage(page.pageId);
+		}
+	};
+
+	actionMap[ACTION_IDS.pinWindow] = async (payload: CustomActionPayload) => {
+		if (payload.callerType === CustomActionCallerType.CustomButton) {
+			const platform = getCurrentSync();
+			const browserWindow = platform.Browser.wrapSync(payload.windowIdentity);
+			const options = await browserWindow.openfinWindow.getOptions();
+			const currentToolbarOptions = (options as BrowserCreateWindowRequest).workspacePlatform.toolbarOptions;
+			await browserWindow.openfinWindow.updateOptions({ alwaysOnTop: true });
+			if (currentToolbarOptions !== undefined && currentToolbarOptions !== null) {
+				const newButtons = await updateToolbarButtons(
+					currentToolbarOptions.buttons,
+					payload.customData.sourceId as string,
+					payload.customData.replacementId as string
+				);
+				await browserWindow.replaceToolbarOptions({ buttons: newButtons });
+			}
+		}
+
+		if (payload.callerType === CustomActionCallerType.ViewTabContextMenu) {
+			const platform = getCurrentSync();
+			const browserWindow = platform.Browser.wrapSync(payload.windowIdentity as OpenFin.Identity);
+			const options = await browserWindow.openfinWindow.getOptions();
+			const currentToolbarOptions = (options as BrowserCreateWindowRequest).workspacePlatform.toolbarOptions;
+			await browserWindow.openfinWindow.updateOptions({ alwaysOnTop: true });
+			if (currentToolbarOptions !== undefined && currentToolbarOptions !== null) {
+				const newButtons = await updateToolbarButtons(
+					currentToolbarOptions.buttons,
+					payload.customData.sourceId as string,
+					payload.customData.replacementId as string
+				);
+				await browserWindow.replaceToolbarOptions({ buttons: newButtons });
+			}
+		}
+	};
+
+	actionMap[ACTION_IDS.unpinWindow] = async (payload: CustomActionPayload) => {
+		if (payload.callerType === CustomActionCallerType.CustomButton) {
+			const platform = getCurrentSync();
+			const browserWindow = platform.Browser.wrapSync(payload.windowIdentity);
+			const options = await browserWindow.openfinWindow.getOptions();
+			const currentToolbarOptions = (options as BrowserCreateWindowRequest).workspacePlatform.toolbarOptions;
+			await browserWindow.openfinWindow.updateOptions({ alwaysOnTop: false });
+			if (currentToolbarOptions !== undefined && currentToolbarOptions !== null) {
+				const newButtons = await updateToolbarButtons(
+					currentToolbarOptions.buttons,
+					payload.customData.sourceId as string,
+					payload.customData.replacementId as string
+				);
+				await browserWindow.replaceToolbarOptions({ buttons: newButtons });
+			}
+		}
+	};
+
+	actionMap[ACTION_IDS.homeShow] = async () => {
+		await show();
+	};
+
+	actionMap[ACTION_IDS.notificationToggle] = async () => {
+		await toggleNotificationCenter();
+	};
+
+	actionMap[ACTION_IDS.share] = async (payload: CustomActionPayload) => {
+		if (payload.callerType === CustomActionCallerType.CustomButton) {
+			await showShareOptions(payload);
+		}
+	};
+
+	actionMap[ACTION_IDS.changeOpacity] = async (payload: CustomActionPayload) => {
+		if (payload.callerType === CustomActionCallerType.CustomButton) {
+			const platform = getCurrentSync();
+			const browserWindow = platform.Browser.wrapSync(payload.windowIdentity);
+			const options = await browserWindow.openfinWindow.getOptions();
+			const currentToolbarOptions = (options as BrowserCreateWindowRequest).workspacePlatform.toolbarOptions;
+			await browserWindow.openfinWindow.updateOptions({ opacity: 0.7 });
+			if (currentToolbarOptions !== undefined && currentToolbarOptions !== null) {
+				const newButtons = await updateToolbarButtons(
+					currentToolbarOptions.buttons,
+					payload.customData.sourceId as string,
+					payload.customData.replacementId as string
+				);
+				await browserWindow.replaceToolbarOptions({ buttons: newButtons });
+			}
+		}
+	};
+
+	actionMap[ACTION_IDS.restoreOpacity] = async (payload: CustomActionPayload) => {
+		if (payload.callerType === CustomActionCallerType.CustomButton) {
+			const platform = getCurrentSync();
+			const browserWindow = platform.Browser.wrapSync(payload.windowIdentity);
+			const options = await browserWindow.openfinWindow.getOptions();
+			const currentToolbarOptions = (options as BrowserCreateWindowRequest).workspacePlatform.toolbarOptions;
+			await browserWindow.openfinWindow.updateOptions({ opacity: 1 });
+
+			if (currentToolbarOptions !== undefined && currentToolbarOptions !== null) {
+				const newButtons = await updateToolbarButtons(
+					currentToolbarOptions.buttons,
+					payload.customData.sourceId as string,
+					payload.customData.replacementId as string
+				);
+				await browserWindow.replaceToolbarOptions({ buttons: newButtons });
+			}
+		}
+	};
+
+	return actionMap;
 }
