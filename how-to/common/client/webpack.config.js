0cbe61589fdb399124524d6d466f259e4b3d1815
John <john.mandia@gmail.com>
2022-07-19 10:59:05 +0100

    Added a basic starter template that isn't included in the apps list (#173)
    * Added a basic starter template that isn't included in the apps list but shows ts and js approaches.
    
    * Html updates

--- how-to/common/client/webpack.config.js
+++ how-to/common/client/webpack.config.js
@@ -27,5 +27,32 @@ module.exports = [
 		experiments: {
 			outputModule: true
 		}
+	},
+	{
+		entry: './client/src/views/template/index.ts',
+		devtool: 'inline-source-map',
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					use: 'ts-loader',
+					exclude: /node_modules/
+				}
+			]
+		},
+		resolve: {
+			extensions: ['.tsx', '.ts', '.js']
+		},
+		externals: { fin: 'fin' },
+		output: {
+			filename: 'index.bundle.js',
+			library: {
+				type: 'module'
+			},
+			path: path.resolve(__dirname, '..', 'public', 'views', 'template', 'ts')
+		},
+		experiments: {
+			outputModule: true
+		}
 	}
 ];
