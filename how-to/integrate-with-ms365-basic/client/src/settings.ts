696f099072aaa0f00c97eee3b97b8ef3923129b6
Martyn Janes <martyn@obany.com>
2022-11-10 15:33:24 +0200

    Update settings

--- how-to/integrate-with-ms365-basic/client/src/settings.ts
+++ how-to/integrate-with-ms365-basic/client/src/settings.ts
@@ -1,3 +1,3 @@
-export const CLIENT_ID: string = "fd7ccadf-dc09-4ed5-aa04-e10855d757f7";
-export const TENANT_ID: string = "f8e6c64e-926f-4470-96b3-fd26345f27ff";
+export const CLIENT_ID: string = "";
+export const TENANT_ID: string = "";
 export const REDIRECT_URL: string = "http://localhost:8080/oauth_redirect.html";
