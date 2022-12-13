09985c7a71812e639a3e572a6e4057ffdfc3afe3
Martyn Janes <martyn@obany.com>
2022-12-13 08:47:31 +0200

    Populate defaults correclty

--- how-to/use-theming/client/src/platform.ts
+++ how-to/use-theming/client/src/platform.ts
@@ -1,40 +1,38 @@
 import { init as workspacePlatformInit } from "@openfin/workspace-platform";
+import type { CustomThemeOptions, CustomThemeOptionsWithScheme } from "@openfin/workspace/client-api-platform/src/shapes";
 import { DEFAULT_PALETTES } from "./defaultPalettes";
 import type { ThemingPayload } from "./shapes";
 
 export async function init(themingPayload?: ThemingPayload): Promise<void> {
 	console.log("Initializing platform");
 
-	let palette;
-	let palettes;
-	if (themingPayload) {
-		if ("palette" in themingPayload) {
-			palette = {
+	let customTheme: CustomThemeOptions | CustomThemeOptionsWithScheme;
+	if (themingPayload && "palette" in themingPayload) {
+		customTheme = {
+			label: "theme",
+			palette: {
 				...DEFAULT_PALETTES.dark,
 				...themingPayload?.palette
-			};
-		} else {
-			palettes = {
+			}
+		};
+	} else {
+		customTheme = {
+			label: "theme",
+			palettes: {
 				dark: {
 					...DEFAULT_PALETTES.dark,
-					...themingPayload.palettes?.dark
+					...themingPayload?.palettes?.dark
 				},
 				light: {
 					...DEFAULT_PALETTES.light,
-					...themingPayload.palettes?.light
+					...themingPayload?.palettes?.light
 				}
-			};
-		}
+			}
+		};
 	}
 
 	await workspacePlatformInit({
 		browser: {},
-		theme: [
-			{
-				label: "theme",
-				palette,
-				palettes
-			}
-		]
+		theme: [customTheme]
 	});
 }
