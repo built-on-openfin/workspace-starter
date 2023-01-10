72c1f56669e8dfdbe78c4b42547fd90f378c2540
johnman <john.mandia@gmail.com>
2023-01-10 13:11:12 +0000

    Deploying to gh-pages from @ built-on-openfin/workspace-starter@a30ce9e6290a754ec2d63a4db0a519bc522392e8 🚀

---
+++ dev/fdc3mapper-update/common/views/contact/investments-and-models/index.js
@@ -0,0 +1,24 @@
+function init() {
+	if (window.fdc3) {
+		const contextHandler = (ctx) => {
+			console.log('Context Received:', ctx);
+			if (ctx.type === 'fdc3.contact') {
+				setContact(ctx);
+			}
+		};
+
+		window.fdc3.addContextListener(contextHandler);
+
+		window.fdc3.addIntentListener('ViewContact', contextHandler);
+	}
+}
+
+function setContact(ctx) {
+	const userNameContainers = document.querySelectorAll('#username');
+
+	for (let i = 0; i < userNameContainers.length; i++) {
+		userNameContainers[i].textContent = ctx.name;
+	}
+}
+
+window.addEventListener('DOMContentLoaded', init);
