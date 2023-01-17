9f1aac9405aca5e392617f5cb938cb2492985e9b
obany <martyn@obany.com>
2023-01-17 05:36:07 +0000

    Deploying to gh-pages from @ built-on-openfin/workspace-starter@36c52b070f2032f585968e6b81a784d2d9790fca 🚀

---
+++ dev/martyn/ws11-store/common/views/contact/participant-summary/index.js
@@ -0,0 +1,35 @@
+function init() {
+	const contextPicker = document.querySelector('#context-group-picker');
+	contextPicker.style.display = fin.me.isWindow ? 'block' : 'none';
+
+	if (window.fdc3) {
+		const contextHandler = (ctx) => {
+			console.log('Context Received:', ctx);
+			if (ctx.type === 'fdc3.contact') {
+				setContact(ctx);
+			}
+		};
+
+		window.fdc3.addContextListener(contextHandler);
+
+		window.fdc3.addIntentListener('ViewContact', contextHandler);
+	}
+}
+
+function setContact(ctx) {
+	const username = ctx.name;
+	const email = ctx.id?.email;
+
+	const userNameContainers = document.querySelectorAll('#username');
+	const emailContainers = document.querySelectorAll('#email');
+
+	for (let i = 0; i < userNameContainers.length; i++) {
+		userNameContainers[i].textContent = username;
+	}
+
+	for (let i = 0; i < emailContainers.length; i++) {
+		emailContainers[i].textContent = email;
+	}
+}
+
+window.addEventListener('DOMContentLoaded', init);
