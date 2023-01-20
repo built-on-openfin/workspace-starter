c80e430f2e3ad469a7129cd3184b114c60baa76e
John <john.mandia@gmail.com>
2023-01-20 09:48:27 +0000

    Merge main into v10
    Cleaning up files that did not get moved.

--- how-to/customize-workspace/client/src/modules/actions/developer/index.ts
+++
@@ -1,6 +0,0 @@
-import type { ModuleImplementation, ModuleTypes } from "customize-workspace/shapes/module-shapes";
-import { DeveloperActions } from "./actions";
-
-export const entryPoints: { [type in ModuleTypes]?: ModuleImplementation } = {
-	actions: new DeveloperActions()
-};
