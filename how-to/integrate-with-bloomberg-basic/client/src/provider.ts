1f480813e32bb56742caa8aebf2f4eabfc07c049
Ameet Jayawant <ameetj85@gmail.com>
2023-03-30 21:39:56 -0400

    logging and minor changes

--- how-to/integrate-with-bloomberg-basic/client/src/provider.ts
+++ how-to/integrate-with-bloomberg-basic/client/src/provider.ts
@@ -17,6 +17,12 @@ let bbgConnection;
 let client;
 let isConnected = false;
 
+// global vars
+let selectedIntent = "";
+let fdc3Denomination = "";
+let bbgMnemonic = "";
+let intentValue = "";
+
 window.addEventListener("DOMContentLoaded", async () => {
 	const platform = fin.Platform.getCurrentSync();
 
@@ -30,6 +36,8 @@ window.addEventListener("DOMContentLoaded", async () => {
 const initDom = () => {
     const btnConnect = document.querySelector("#btnConnect");
     const btnClearLogs = document.querySelector("#btnClear");
+    const btnQuery = document.querySelector("#btnQuery");
+    const inputText = document.querySelector("#intentValue");
     const selectChoice = document.querySelector("#intentName");
 
     let selectOptions: HTMLSelectElement;
@@ -39,32 +47,50 @@ const initDom = () => {
 
     btnConnect.addEventListener("click", connectToBBGTerminal);
     btnClearLogs.addEventListener("click", clearLogs);
+    btnQuery.addEventListener("click", createAndFireIntent);
+    inputText.addEventListener("input", getInputValue);
+
     selectChoice.addEventListener("change", function handleChange(event) {
         switch(this.value) {
             case "ViewChart":
                 // document.querySelector("#selectChoice").innerHTML = "Intent to be fired is ViewChart. Content Type is fdc3.instrument. Bloomberg Terminal Mnemonic: GP";
                 logInformation("Intent to be fired is ViewChart. Content Type is fdc3.instrument. Bloomberg Terminal Mnemonic: GP");
+                selectedIntent = "ViewChart";
+                fdc3Denomination = "fdc3.instrument";
+                bbgMnemonic = "GP";
                 break;
             case "ViewContact":
-                // document.querySelector("#selectChoice").innerHTML = "Intent to be fired is ViewContact. Content Type is fdc3.contact. Bloomberg Terminal Mnemonic: BIO";
                 logInformation("Intent to be fired is ViewContact. Content Type is fdc3.contact. Bloomberg Terminal Mnemonic: BIO");
+                selectedIntent = "ViewContact";
+                fdc3Denomination = "fdc3.contact";
+                bbgMnemonic = "BIO";
                 break;
             case "ViewInstrument":
-                // document.querySelector("#selectChoice").innerHTML = "Intent to be fired is ViewInstrument. Content Type is fdc3.instrument. Bloomberg Terminal Mnemonic: DES";
                 logInformation("Intent to be fired is ViewInstrument. Content Type is fdc3.instrument. Bloomberg Terminal Mnemonic: DES");
+                selectedIntent = "ViewInstrument";
+                fdc3Denomination = "fdc3.instrument";
+                bbgMnemonic = "DES";
                 break;
             case "ViewQuote":
-                // document.querySelector("#selectChoice").innerHTML = "Intent to be fired is ViewQuote. Content Type is fdc3.instrument. Bloomberg Terminal Mnemonic: Q";
                 logInformation("Intent to be fired is ViewQuote. Content Type is fdc3.instrument. Bloomberg Terminal Mnemonic: Q");
+                selectedIntent = "ViewQuote";
+                fdc3Denomination = "fdc3.instrument";
+                bbgMnemonic = "Q";
                 break;
         }
     });
 };
 
+const getInputValue = (e) => {
+    const inputVal = e.target.value;
+    intentValue = inputVal;
+    console.log(inputVal);
+}
 
 // Connect to Bloomberg Terminal
 const connectToBBGTerminal = async (): Promise<void> => {
     try {
+        logInformation("Checking Bloomberg Terminal Status");
         if (!(await isBloombergTerminalReady())) {
             const error = new Error("Failed to connect to Bloomberg terminal.");
             console.log(error);
@@ -73,7 +99,7 @@ const connectToBBGTerminal = async (): Promise<void> => {
           }
 
         bbgConnection = await connect();
-
+        logInformation("Connection successful"); 
         isConnected = true;
     } catch (error) {
         console.log(error);
@@ -89,6 +115,21 @@ const connectToBroker = async () => {
     client = fin.Interop.connectSync("openfin", interopConfig);
 };
 
+const createAndFireIntent = (e) => {
+    logInformation(`action: ${selectedIntent}, type: ${fdc3Denomination}, bbg mnemonic: ${bbgMnemonic}, search value: ${intentValue}`);
+    // Define the intent to fire
+    /*
+    const intent = {
+        action: "ViewChart",
+        dataType: "Stock",
+        data: {
+          symbol: "AAPL"
+        }
+      };
+
+      fireIntentforBBG(intent);
+      */
+};
 
 // Fire an intent
 const fireIntentforBBG = (intent) => {
@@ -97,7 +138,7 @@ const fireIntentforBBG = (intent) => {
     }
 };
 
-export function logInformation(info: string) {
+const logInformation = (info: string) => {
     const logElem = document.querySelector("#logOutput");
 
     if(logElem) {
