1afca02ba866de408ead8b222ca303ea8f8d0b6d
joeransegnola <129192453+joeransegnola@users.noreply.github.com>
2023-04-04 12:57:30 +0000

    Deploying to gh-pages from @ built-on-openfin/workspace-starter@17f668e82b9f4db9d4ce1134c4be41c301e46582 🚀

---
+++ dev/joe/update-readme/common/views/contact/participant-history/index.js
@@ -0,0 +1,28 @@
+function init() {
+	if (window.fdc3) {
+		const contextHandler = (ctx) => {
+			console.log('Context Received:', ctx);
+			if (ctx.type === 'fdc3.contact') {
+				setContact(ctx);
+			}
+		};
+
+		window.fdc3.addContextListener(contextHandler);
+
+		window.fdc3.addIntentListener('ViewContact', contextHandler);
+	}
+}
+
+function setContact(ctx) {
+	const randomId = (Math.random() * 1000000).toFixed(0);
+
+	const userNameContainers = document.querySelectorAll('.username');
+
+	const username = `${ctx.name} (${randomId})`;
+
+	for (let i = 0; i < userNameContainers.length; i++) {
+		userNameContainers[i].textContent = username;
+	}
+}
+
+window.addEventListener('DOMContentLoaded', init);
