26a70730da1d21dc154f8e8a00d254a48e6707e2
Martyn Janes <martyn@obany.com>
2023-04-18 14:30:03 +0200

    Separate initializeDOM

--- how-to/register-with-home-basic/client/src/provider.ts
+++ how-to/register-with-home-basic/client/src/provider.ts
@@ -11,7 +11,44 @@ window.addEventListener("DOMContentLoaded", async () => {
 	// Provide default icons and default theme for the browser windows
 	await initializeWorkspacePlatform(PLATFORM_ICON);
 
-	// Get the DOM elements from the provider.html page
+	// Get the DOM elements from the provider.html page and initialize them
+	await initializeDOM();
+});
+
+/**
+ * Initialize the workspace platform.
+ * @param icon The icon to use in windows.
+ */
+async function initializeWorkspacePlatform(icon: string): Promise<void> {
+	console.log("Initialising workspace platform");
+	await init({
+		browser: {
+			defaultWindowOptions: {
+				icon,
+				workspacePlatform: {
+					pages: [],
+					favicon: icon
+				}
+			}
+		},
+		theme: [
+			{
+				label: "Default",
+				default: "dark",
+				palette: {
+					brandPrimary: "#0A76D3",
+					brandSecondary: "#383A40",
+					backgroundPrimary: "#1E1F23"
+				}
+			}
+		]
+	});
+}
+
+/**
+ * Initialize the DOM elements.
+ */
+async function initializeDOM(): Promise<void> {
 	const registerButton = document.querySelector<HTMLButtonElement>("#register");
 	const deregisterButton = document.querySelector<HTMLButtonElement>("#deregister");
 	const showButton = document.querySelector<HTMLButtonElement>("#show");
@@ -67,34 +104,4 @@ window.addEventListener("DOMContentLoaded", async () => {
 			}
 		});
 	}
-});
-
-/**
- * Initialize the workspace platform.
- * @param icon The icon to use in windows.
- */
-async function initializeWorkspacePlatform(icon: string): Promise<void> {
-	console.log("Initialising workspace platform");
-	await init({
-		browser: {
-			defaultWindowOptions: {
-				icon,
-				workspacePlatform: {
-					pages: [],
-					favicon: icon
-				}
-			}
-		},
-		theme: [
-			{
-				label: "Default",
-				default: "dark",
-				palette: {
-					brandPrimary: "#0A76D3",
-					brandSecondary: "#383A40",
-					backgroundPrimary: "#1E1F23"
-				}
-			}
-		]
-	});
 }
