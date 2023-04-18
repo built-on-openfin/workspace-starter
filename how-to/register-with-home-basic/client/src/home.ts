246bb10e9832a06b0cf769ecda2f080f96b62526
Martyn Janes <martyn@obany.com>
2023-04-18 16:15:07 +0200

    Register with Home Basic - Simplify code (#413)
    * Simplify example
    
    * Separate initializeDOM

--- how-to/register-with-home-basic/client/src/home.ts
+++ how-to/register-with-home-basic/client/src/home.ts
@@ -1,146 +1,136 @@
 import {
-	CLIDispatchedSearchResult,
-	CLIProvider,
-	CLISearchListenerRequest,
-	CLISearchListenerResponse,
-	CLISearchResponse,
+	type CLIDispatchedSearchResult,
+	type CLIProvider,
+	type CLISearchListenerRequest,
+	type CLISearchResponse,
 	CLITemplate,
 	Home,
-	HomeRegistration,
-	HomeSearchResult
+	type HomeRegistration,
+	type HomeSearchResult,
+	type App
 } from "@openfin/workspace";
-import { getCurrentSync } from "@openfin/workspace-platform";
-import { getApps } from "./apps";
+import { getApps, launchApp } from "./apps";
 
-const providerId = "register-with-home-basic";
-
-async function getResults(queryLower: string): Promise<CLISearchResponse> {
-	const apps = await getApps();
-
-	if (Array.isArray(apps)) {
-		const initialResults: HomeSearchResult[] = [];
-
-		for (let i = 0; i < apps.length; i++) {
-			const entry: Partial<HomeSearchResult> = {
-				key: apps[i].appId,
-				title: apps[i].title,
-				data: apps[i],
-				actions: []
-			};
-			const action = { name: "Launch View", hotkey: "enter" };
-
-			if (apps[i].manifestType === "view") {
-				entry.label = "View";
-				entry.actions = [action];
-			}
-			if (apps[i].manifestType === "snapshot") {
-				entry.label = "Snapshot";
-				action.name = "Launch Snapshot";
-				entry.actions = [action];
-			}
-			if (apps[i].manifestType === "manifest") {
-				entry.label = "App";
-				action.name = "Launch App";
-				entry.actions = [action];
-			}
-			if (apps[i].manifestType === "external") {
-				action.name = "Launch Native App";
-				entry.actions = [action];
-				entry.label = "Native App";
-			}
-
-			if (Array.isArray(apps[i].icons) && apps[i].icons.length > 0) {
-				entry.icon = apps[i].icons[0].src;
-			}
-
-			if (apps[i].description !== undefined) {
-				entry.description = apps[i].description;
-				entry.shortDescription = apps[i].description;
-				entry.template = CLITemplate.SimpleText;
-				entry.templateContent = apps[i].description;
-			} else {
-				entry.template = CLITemplate.Plain;
-			}
-			initialResults.push(entry as HomeSearchResult);
-		}
-
-		let finalResults;
-
-		if (queryLower === undefined || queryLower === null || queryLower.length === 0) {
-			finalResults = initialResults;
-		} else {
-			finalResults = initialResults.filter((entry) => {
-				const targetValue = entry.title;
-
-				if (targetValue !== undefined && targetValue !== null && typeof targetValue === "string") {
-					return targetValue.toLowerCase().includes(queryLower);
-				}
-				return false;
-			});
-		}
-
-		const response: CLISearchResponse = {
-			results: finalResults
-		};
-
-		return response;
-	}
-	return {
-		results: []
-	};
-}
-
-export async function register(): Promise<HomeRegistration> {
+/**
+ * Register with the home component.
+ * @param id The id to register the provider with.
+ * @param title The title to use for the home registration.
+ * @param icon The icon to use for the home registration.
+ * @returns The registration details for home.
+ */
+export async function register(id: string, title: string, icon: string): Promise<HomeRegistration> {
 	console.log("Initialising home.");
 
-	const queryMinLength = 3;
-
-	const onUserInput = async (
-		request: CLISearchListenerRequest,
-		response: CLISearchListenerResponse
-	): Promise<CLISearchResponse> => {
+	// The callback fired when the user types in the home query
+	const onUserInput = async (request: CLISearchListenerRequest): Promise<CLISearchResponse> => {
 		const queryLower = request.query.toLowerCase();
+
+		// If the query starts with a / treat this as a help request
+		// so we don't have any additional entries to show
 		if (queryLower.startsWith("/")) {
 			return { results: [] };
-		}
-
-		if (queryLower.length < queryMinLength) {
-			return getResults("");
 		}
 
 		return getResults(queryLower);
 	};
 
-	const onSelection = async (result: CLIDispatchedSearchResult) => {
+	// The callback fired when a selection is made in home
+	const onSelection = async (result: CLIDispatchedSearchResult): Promise<void> => {
 		if (result.data !== undefined) {
-			const platform = getCurrentSync();
-			await platform.launchApp({ app: result.data });
+			await launchApp(result.data as App);
 		} else {
 			console.warn("Unable to execute result without data being passed");
 		}
 	};
 
 	const cliProvider: CLIProvider = {
-		title: "Basic Workspace Platform",
-		id: providerId,
-		icon: "http://localhost:8080/favicon.ico",
+		id,
+		title,
+		icon,
 		onUserInput,
 		onResultDispatch: onSelection
 	};
 
+	const homeRegistration = await Home.register(cliProvider);
 	console.log("Home configured.");
+	console.log(homeRegistration);
 
-	return Home.register(cliProvider);
+	return homeRegistration;
 }
 
-export async function deregister() {
-	return Home.deregister(providerId);
+/**
+ * Get the list of results to show in home.
+ * @param queryLower Lower case version of query.
+ * @returns The search response containing results.
+ */
+async function getResults(queryLower: string): Promise<CLISearchResponse> {
+	const apps = await getApps();
+
+	let filteredApps: App[];
+
+	// If the query length is less than 3 don't do any filtering
+	if (queryLower.length < 3) {
+		filteredApps = apps;
+	} else {
+		// Otherwise try and match the app title with the query
+		filteredApps = apps.filter((entry) => entry.title.toLowerCase().includes(queryLower));
+	}
+
+	return {
+		results: mapAppEntriesToSearchEntries(filteredApps)
+	};
 }
 
-export async function show() {
-	return Home.show();
-}
+/**
+ * Convert the app definitions into search results.
+ * @param apps The list of apps to convert.
+ * @returns The search result templates.
+ */
+function mapAppEntriesToSearchEntries(apps: App[]): HomeSearchResult[] {
+	const results: HomeSearchResult[] = [];
 
-export async function hide() {
-	return Home.hide();
+	if (Array.isArray(apps)) {
+		for (const app of apps) {
+			const action = { name: "Launch View", hotkey: "enter" };
+			const entry: Partial<HomeSearchResult> = {
+				key: app.appId,
+				title: app.title,
+				data: app
+			};
+
+			if (app.manifestType === "view") {
+				entry.label = "View";
+				entry.actions = [action];
+			} else if (app.manifestType === "snapshot") {
+				entry.label = "Snapshot";
+				action.name = "Launch Snapshot";
+				entry.actions = [action];
+			} else if (app.manifestType === "manifest") {
+				entry.label = "App";
+				action.name = "Launch App";
+				entry.actions = [action];
+			} else if (app.manifestType === "external") {
+				action.name = "Launch Native App";
+				entry.actions = [action];
+				entry.label = "Native App";
+			}
+
+			if (Array.isArray(app.icons) && app.icons.length > 0) {
+				entry.icon = app.icons[0].src;
+			}
+
+			if (app.description !== undefined) {
+				entry.description = app.description;
+				entry.shortDescription = app.description;
+				entry.template = CLITemplate.SimpleText;
+				entry.templateContent = app.description;
+			} else {
+				entry.template = CLITemplate.Plain;
+			}
+
+			results.push(entry as HomeSearchResult);
+		}
+	}
+
+	return results;
 }
--- how-to/register-with-home-basic/client/src/provider.ts
+++ how-to/register-with-home-basic/client/src/provider.ts
@@ -1,64 +1,107 @@
-import type { HomeRegistration } from "@openfin/workspace";
-import { init as workspacePlatformInit } from "@openfin/workspace-platform";
-import { register, show, hide, deregister } from "./home";
+import { Home, type HomeRegistration } from "@openfin/workspace";
+import { init } from "@openfin/workspace-platform";
+import { register } from "./home";
 
-async function init() {
-	await workspacePlatformInit({
-		browser: {}
-	});
-	const registerHome = document.querySelector<HTMLButtonElement>("#register");
-	const deregisterHome = document.querySelector<HTMLButtonElement>("#deregister");
-	const showHome = document.querySelector<HTMLButtonElement>("#show");
-	const hideHome = document.querySelector<HTMLButtonElement>("#hide");
-	const query = document.querySelector<HTMLInputElement>("#query");
-	const go = document.querySelector<HTMLButtonElement>("#go");
+const PLATFORM_ID = "register-with-home-basic";
+const PLATFORM_TITLE = "Register With Home Basic";
+const PLATFORM_ICON = "http://localhost:8080/favicon.ico";
 
-	showHome.disabled = true;
-	hideHome.disabled = true;
-	deregisterHome.disabled = true;
-	query.disabled = true;
-	go.disabled = true;
+window.addEventListener("DOMContentLoaded", async () => {
+	// The DOM is ready so initialize the platform
+	// Provide default icons and default theme for the browser windows
+	await initializeWorkspacePlatform(PLATFORM_ICON);
 
-	let homeRegistration: HomeRegistration;
+	// Get the DOM elements from the provider.html page and initialize them
+	await initializeDOM();
+});
 
-	registerHome.addEventListener("click", async () => {
-		homeRegistration = await register();
-		showHome.disabled = false;
-		hideHome.disabled = false;
-		deregisterHome.disabled = false;
-		registerHome.disabled = true;
-		query.disabled = false;
-		go.disabled = false;
-	});
-
-	deregisterHome.addEventListener("click", async () => {
-		showHome.disabled = true;
-		hideHome.disabled = true;
-		deregisterHome.disabled = true;
-		registerHome.disabled = false;
-		query.disabled = true;
-		go.disabled = true;
-		homeRegistration = undefined;
-		query.value = "";
-		await deregister();
-	});
-
-	showHome.addEventListener("click", async () => {
-		await show();
-	});
-
-	hideHome.addEventListener("click", async () => {
-		await hide();
-	});
-
-	go.addEventListener("click", async () => {
-		if (homeRegistration) {
-			await show();
-			await homeRegistration.setSearchQuery(query.value);
-		}
+/**
+ * Initialize the workspace platform.
+ * @param icon The icon to use in windows.
+ */
+async function initializeWorkspacePlatform(icon: string): Promise<void> {
+	console.log("Initialising workspace platform");
+	await init({
+		browser: {
+			defaultWindowOptions: {
+				icon,
+				workspacePlatform: {
+					pages: [],
+					favicon: icon
+				}
+			}
+		},
+		theme: [
+			{
+				label: "Default",
+				default: "dark",
+				palette: {
+					brandPrimary: "#0A76D3",
+					brandSecondary: "#383A40",
+					backgroundPrimary: "#1E1F23"
+				}
+			}
+		]
 	});
 }
 
-window.addEventListener("DOMContentLoaded", async () => {
-	await init();
-});
+/**
+ * Initialize the DOM elements.
+ */
+async function initializeDOM(): Promise<void> {
+	const registerButton = document.querySelector<HTMLButtonElement>("#register");
+	const deregisterButton = document.querySelector<HTMLButtonElement>("#deregister");
+	const showButton = document.querySelector<HTMLButtonElement>("#show");
+	const hideButton = document.querySelector<HTMLButtonElement>("#hide");
+	const queryInput = document.querySelector<HTMLInputElement>("#query");
+	const goButton = document.querySelector<HTMLButtonElement>("#go");
+
+	if (registerButton && deregisterButton && showButton && hideButton && queryInput && goButton) {
+		let homeRegistration: HomeRegistration | undefined;
+
+		// Disable all the buttons until the platform is registered
+		showButton.disabled = true;
+		hideButton.disabled = true;
+		deregisterButton.disabled = true;
+		queryInput.disabled = true;
+		goButton.disabled = true;
+
+		// Wire up the button click event listeners
+		registerButton.addEventListener("click", async () => {
+			homeRegistration = await register(PLATFORM_ID, PLATFORM_TITLE, PLATFORM_ICON);
+			showButton.disabled = false;
+			hideButton.disabled = false;
+			deregisterButton.disabled = false;
+			registerButton.disabled = true;
+			queryInput.disabled = false;
+			goButton.disabled = false;
+		});
+
+		deregisterButton.addEventListener("click", async () => {
+			showButton.disabled = true;
+			hideButton.disabled = true;
+			deregisterButton.disabled = true;
+			registerButton.disabled = false;
+			queryInput.disabled = true;
+			goButton.disabled = true;
+			homeRegistration = undefined;
+			queryInput.value = "";
+			await Home.deregister(PLATFORM_ID);
+		});
+
+		showButton.addEventListener("click", async () => {
+			await Home.show();
+		});
+
+		hideButton.addEventListener("click", async () => {
+			await Home.hide();
+		});
+
+		goButton.addEventListener("click", async () => {
+			if (homeRegistration) {
+				await Home.show();
+				await homeRegistration.setSearchQuery(queryInput.value);
+			}
+		});
+	}
+}
