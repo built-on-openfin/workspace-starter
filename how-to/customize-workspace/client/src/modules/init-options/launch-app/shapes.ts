a5700a6fcc38081f8903c69696355e711bfad8aa
John <john.mandia@gmail.com>
2023-05-04 21:37:30 +0100

    Update launch app init options to support permissioned list.

--- how-to/customize-workspace/client/src/modules/init-options/launch-app/shapes.ts
+++ how-to/customize-workspace/client/src/modules/init-options/launch-app/shapes.ts
@@ -1,3 +1,9 @@
+import { type InitOptionsHandlerOptions } from "customize-workspace/shapes";
+
 export interface LaunchAppPayload {
 	appId: string;
+}
+
+export interface LaunchAppOptions extends InitOptionsHandlerOptions {
+	supportedManifestTypes: string[];
 }
