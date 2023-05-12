a697ec2dabdc8429a79c712a2b45cdf97b50e5df
Martyn Janes <martyn@obany.com>
2023-05-12 10:33:24 +0200

    Simplify the integration with excel example

--- how-to/integrate-with-excel/client/src/integrations/excel/index.ts
+++
@@ -1,5 +0,0 @@
-import { ExcelIntegrationProvider } from "./integration-provider";
-
-export const entryPoints: { [id: string]: ExcelIntegrationProvider } = {
-	integrations: new ExcelIntegrationProvider()
-};
