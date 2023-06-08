61d2ce3c1e2f1b314b91d805cad58942de2bda70
John <john.mandia@gmail.com>
2023-06-08 15:10:28 +0100

    Added a more advanced msf365 example

--- how-to/integrate-with-ms365-microflow/README.md
+++ how-to/integrate-with-ms365-microflow/README.md
@@ -3,9 +3,9 @@
 > **_:information_source: OpenFin Workspace:_** [OpenFin Workspace](https://www.openfin.co/workspace/) is a commercial product and this repo is for evaluation purposes (See [LICENSE.MD](LICENSE.MD)). Use of the OpenFin Container and OpenFin Workspace components is only granted pursuant to a license from OpenFin (see [manifest](public/manifest.fin.json)). Please [**contact us**](https://www.openfin.co/workspace/poc/) if you would like to request a developer evaluation key or to discuss a production license.
 > OpenFin Workspace is currently **only supported on Windows** although you can run the sample on a Mac for development purposes.
 
-# Integrate With Microsoft 365 Microflows - Basic
+# Integrate With Microsoft 365 Microflows
 
-OpenFin Workspace empowers you to browse and search your Microsoft 365 org data using OpenFin Home, via our Microsoft 365 integration API. OpenFin Home provides you with a fast and intuitive interface for discovering your Microsoft data. Microflows take this one step further by giving you an easy way to enable this integration through a few lines of code and some config.
+OpenFin Workspace empowers you to browse and search your Microsoft 365 org data using OpenFin Home, via our Microsoft 365 integration API. OpenFin Home provides you with a fast and intuitive interface for discovering your Microsoft data. Microflows take this one step further by giving you an easy way to enable this integration through a few lines of code and some config. This example shows you using the MicroFlow alongside your own Home registration.
 
 ## Running the Sample
 
---
+++ how-to/integrate-with-ms365-microflow/client/src/provider.ts
@@ -0,0 +1,114 @@
+import type OpenFin from "@openfin/core";
+import {
+	Home,
+	type HomeDispatchedSearchResult,
+	type HomeProvider,
+	type HomeSearchListenerRequest,
+	type HomeSearchListenerResponse,
+	type HomeSearchResponse,
+	Integrations
+} from "@openfin/workspace";
+import { init } from "@openfin/workspace-platform";
+import { type WorkflowIntegration } from "@openfin/workspace-platform/client-api/src/shapes/integrations";
+import { type CustomSettings } from "./shapes";
+
+const PLATFORM_ICON = "http://localhost:8080/favicon.ico";
+
+window.addEventListener("DOMContentLoaded", async () => {
+	// The DOM is ready so initialize the platform
+	await initializeWorkspacePlatform();
+});
+
+/**
+ * Initialize the workspace platform.
+ */
+async function initializeWorkspacePlatform(): Promise<void> {
+	console.log("Initializing workspace platform");
+	const customSettings = await getManifestCustomSettings();
+	const integrations: WorkflowIntegration[] = [];
+	let msft365WorkflowIntegration;
+	if (customSettings.ms365) {
+		msft365WorkflowIntegration = new Integrations.Microsoft365WorkflowIntegration({
+			connect: {
+				clientId: customSettings.ms365.clientId,
+				redirectUri: customSettings.ms365.redirectUri,
+				tenantId: customSettings.ms365.tenantId
+			},
+			workflows: {
+				search: {
+					disableAutoInitialize: true
+				}
+			}
+		});
+		integrations.push(msft365WorkflowIntegration as unknown as WorkflowIntegration);
+	}
+	await init({
+		browser: {
+			defaultWindowOptions: {
+				icon: PLATFORM_ICON,
+				workspacePlatform: {
+					pages: [],
+					favicon: PLATFORM_ICON
+				}
+			}
+		},
+		integrations
+	});
+	await registerPlatformHomeIntegration();
+	await msft365WorkflowIntegration?.initializeWorkflow("search");
+	await Home.show();
+}
+
+/**
+ * The callback fired when the user types in the home query.
+ * @param request The request object from the home component.
+ * @param response The response to use for async updates.
+ * @returns The results to display in home.
+ */
+async function onUserInput(
+	request: HomeSearchListenerRequest,
+	response: HomeSearchListenerResponse
+): Promise<HomeSearchResponse> {
+	console.log("Home query received.", request.query);
+	return { results: [] };
+}
+
+/**
+ * The callback fired when a selection is made in home.
+ * @param result The item that was selected in home.
+ */
+async function onSelection(result: HomeDispatchedSearchResult): Promise<void> {
+	console.log("Home user action on returned results");
+}
+
+/**
+ * Register the platform's custom home registration
+ */
+async function registerPlatformHomeIntegration() {
+	const id = "my-custom-home-registration";
+	const title = "Custom Platform Home Registration";
+	const icon = PLATFORM_ICON;
+	const homeProvider: HomeProvider = {
+		id,
+		title,
+		icon,
+		onUserInput,
+		onResultDispatch: onSelection
+	};
+
+	await Home.register(homeProvider);
+	console.log("Home configured.");
+}
+
+/**
+ * Read the custom settings from the manifest.fin.json.
+ * @returns The custom settings from the manifest. These settings could come from a service instead.
+ */
+async function getManifestCustomSettings(): Promise<CustomSettings> {
+	// Get the manifest for the current application
+	const app = await fin.Application.getCurrent();
+
+	// Extract the custom settings for this application
+	const manifest: OpenFin.Manifest & { customSettings?: CustomSettings } = await app.getManifest();
+	return manifest.customSettings ?? {};
+}
