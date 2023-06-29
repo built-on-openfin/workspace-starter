87164a72403066bdf468ceb4021c80b42aff0b69
Martyn Janes <martyn@obany.com>
2023-06-29 12:42:51 +0200

    Fix prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -513,7 +513,7 @@ module.exports = {
 		{
 			extends: [
 				'plugin:@typescript-eslint/recommended',
-    			'plugin:@typescript-eslint/recommended-requiring-type-checking',
+				'plugin:@typescript-eslint/recommended-requiring-type-checking',
 				'plugin:@typescript-eslint/strict'
 			],
 			files: ['*.ts'],
