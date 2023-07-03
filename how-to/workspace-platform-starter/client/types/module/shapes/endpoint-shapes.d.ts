21920a0dc463204d3c5c45223c99eab8825cb6c1
Martyn Janes <martyn@obany.com>
2023-07-03 16:01:35 +0400

    Add support for additional verbs in fetch endpoints (#493)

--- how-to/workspace-platform-starter/client/types/module/shapes/endpoint-shapes.d.ts
+++ how-to/workspace-platform-starter/client/types/module/shapes/endpoint-shapes.d.ts
@@ -122,7 +122,7 @@ export interface FetchOptions {
 	/**
 	 * The HTTP methods for fetch.
 	 */
-	method?: "GET" | "POST";
+	method?: "GET" | "POST" | "DELETE" | "PUT" | "PATCH";
 	/**
 	 * Credentials to send with the fetch.
 	 */
