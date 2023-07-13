3155608941927142f18411c1829d616152a7650f
obany <martyn@obany.com>
2023-07-13 05:41:23 +0000

    Deploying to gh-pages from @ built-on-openfin/workspace-starter@39343be7ab7a693d331fdee1df4260e100ce48cc 🚀

--- workspace/v13.1.0/register-with-store/common/views/contact/participant-summary/index.js
+++ workspace/v13.1.0/register-with-store/common/views/contact/participant-summary/index.js
@@ -1,11 +1,17 @@
+import * as usersModule from '../common/contacts.js';
+
+window.addEventListener('DOMContentLoaded', initializeDOM);
+
 /**
  * Initialize the DOM.
  */
-function initializeDOM() {
+async function initializeDOM() {
 	const contextPicker = document.querySelector('#context-group-picker');
 	contextPicker.style.display = fin.me.isWindow ? 'block' : 'none';
 
 	if (window.fdc3) {
+		await usersModule.initialize();
+
 		window.fdc3.addContextListener(contextHandler);
 
 		window.fdc3.addIntentListener('ViewContact', contextHandler);
@@ -32,6 +38,28 @@ function setContact(ctx) {
 	const username = ctx.name;
 	const email = ctx.id?.email;
 
+	const user = usersModule.findUserByEmail(email);
+
+	if (user) {
+		const profilePic = document.querySelector('#profile-pic');
+		profilePic.src = usersModule.getProfilePic(user);
+
+		const ssn = document.querySelector('#ssn');
+		ssn.textContent = user.ssn;
+
+		const gender = document.querySelector('#gender');
+		gender.textContent = user.gender;
+
+		const age = document.querySelector('#age');
+		age.textContent = user.age;
+
+		const dob = document.querySelector('#dob');
+		dob.textContent = user.age;
+
+		const maritalStatus = document.querySelector('#marital-status');
+		maritalStatus.textContent = user.maritalStatus;
+	}
+
 	const userNameContainers = document.querySelectorAll('#username');
 	const emailContainers = document.querySelectorAll('#email');
 
@@ -43,5 +71,3 @@ function setContact(ctx) {
 		element.textContent = email;
 	}
 }
-
-window.addEventListener('DOMContentLoaded', initializeDOM);
