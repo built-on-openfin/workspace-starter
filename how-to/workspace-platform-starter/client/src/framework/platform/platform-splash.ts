ab754b8dd4c9d9c8acfb10be89524afc611c0eaa
Martyn Janes <martyn@obany.com>
2023-07-31 16:09:51 +0200

    Use custom theme and icon

--- how-to/workspace-platform-starter/client/src/framework/platform/platform-splash.ts
+++ how-to/workspace-platform-starter/client/src/framework/platform/platform-splash.ts
@@ -1,4 +1,5 @@
 import type OpenFin from "@openfin/core";
+import { getManifestCustomSettings } from "../settings";
 
 let win: OpenFin.Window | undefined;
 
@@ -6,6 +7,11 @@ let win: OpenFin.Window | undefined;
  * Open a splash screen.
  */
 export async function open(): Promise<void> {
+	const app = await fin.Application.getCurrent();
+	const manifest = await app.getManifest();
+
+	const customSettings = await getManifestCustomSettings();
+
 	win = await fin.Window.create({
 		name: "platform-splash",
 		uuid: "platform-splash",
@@ -14,15 +20,55 @@ export async function open(): Promise<void> {
 		maximizable: false,
 		minimizable: false,
 		frame: false,
-		autoShow: true,
+		autoShow: false,
 		defaultCentered: true,
 		defaultHeight: 130,
-		defaultWidth: 300,
+		defaultWidth: 400,
 		includeInSnapshots: false,
 		resizable: false,
 		saveWindowState: false,
 		showTaskbarIcon: false
 	});
+
+	if (win) {
+		const webWin = win.getWebWindow();
+		if (webWin) {
+			if (manifest.platform?.icon) {
+				const icon = webWin.document.querySelector<HTMLImageElement>("#icon");
+				if (icon) {
+					icon.src = manifest.platform.icon;
+				}
+			}
+
+			if (customSettings?.themeProvider?.themes?.length) {
+				const theme = customSettings.themeProvider.themes[0];
+				if ("palettes" in theme) {
+					const palette =
+						theme.palettes[theme.default ?? (Object.keys(theme.palettes)[0] as "dark" | "light")];
+					if (palette?.backgroundPrimary) {
+						webWin.document.body.style.backgroundColor = palette?.backgroundPrimary;
+
+						if (palette?.textDefault) {
+							const header = webWin.document.querySelector<HTMLElement>("#header");
+							if (header) {
+								header.style.color = palette.textDefault;
+								header.textContent = manifest.shortcut?.name ?? "OpenFin";
+							}
+							const progressLabel = webWin.document.querySelector<HTMLElement>("#progressLabel");
+							if (progressLabel) {
+								progressLabel.style.color = palette.textDefault;
+							}
+							const progress = webWin.document.querySelector<HTMLElement>("#progress");
+							if (progress) {
+								progress.style.color = palette.textDefault;
+							}
+						}
+					}
+				}
+			}
+		}
+		await win.show();
+	}
 }
 
 /**
