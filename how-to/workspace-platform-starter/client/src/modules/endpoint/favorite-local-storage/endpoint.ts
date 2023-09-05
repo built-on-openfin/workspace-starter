824c425bfdabade4612be8ccd162e1fc08e77265
John <john.mandia@gmail.com>
2023-09-05 12:24:17 +0100

    Added example module for storing favorites

--- how-to/workspace-platform-starter/client/src/modules/endpoint/favorite-local-storage/endpoint.ts
+++ how-to/workspace-platform-starter/client/src/modules/endpoint/favorite-local-storage/endpoint.ts
@@ -1,5 +1,13 @@
 import type { Endpoint, EndpointDefinition } from "workspace-platform-starter/shapes/endpoint-shapes";
-import { type EndpointFavoriteEntry, type EndpointFavoriteGetRequest, type EndpointFavoriteGetResponse, type EndpointFavoriteListRequest, type EndpointFavoriteListResponse, type EndpointFavoriteRemoveRequest, type EndpointFavoriteSetRequest, FavoriteEntry } from "workspace-platform-starter/shapes/favorite-shapes";
+import type {
+	EndpointFavoriteEntry,
+	EndpointFavoriteGetRequest,
+	EndpointFavoriteGetResponse,
+	EndpointFavoriteListRequest,
+	EndpointFavoriteListResponse,
+	EndpointFavoriteRemoveRequest,
+	EndpointFavoriteSetRequest
+} from "workspace-platform-starter/shapes/favorite-shapes";
 import type { Logger, LoggerCreator } from "workspace-platform-starter/shapes/logger-shapes";
 import type { ModuleDefinition, ModuleHelpers } from "workspace-platform-starter/shapes/module-shapes";
 import { isEmpty } from "workspace-platform-starter/utils";
@@ -61,15 +69,21 @@ export class FavoriteLocalStorageProvider implements Endpoint<unknown> {
 	 * @param request The request to process.
 	 * @returns True if processed.
 	 */
-	public async action(endpointDefinition: EndpointDefinition,
-		request?: EndpointFavoriteSetRequest | EndpointFavoriteRemoveRequest): Promise<boolean> {
-		if(endpointDefinition.type === "module" && !isEmpty(request)) {
-			if("payload" in request) {
+	public async action(
+		endpointDefinition: EndpointDefinition,
+		request?: EndpointFavoriteSetRequest | EndpointFavoriteRemoveRequest
+	): Promise<boolean> {
+		if (endpointDefinition.type === "module" && !isEmpty(request)) {
+			if ("payload" in request) {
 				return this.requestResponseSetFavorite(request);
 			}
 			return this.requestResponseRemoveFavorite(request);
 		}
-		throw new Error(`This endpoint module only supports the type 'module' and being passed a request. The following endpoint type was specified: ${endpointDefinition.type} and request passed: ${!isEmpty(request)}`);
+		throw new Error(
+			`This endpoint module only supports the type 'module' and being passed a request. The following endpoint type was specified: ${
+				endpointDefinition.type
+			} and request passed: ${!isEmpty(request)}`
+		);
 	}
 
 	/**
@@ -78,16 +92,21 @@ export class FavoriteLocalStorageProvider implements Endpoint<unknown> {
 	 * @param request The request to process.
 	 * @returns The response to the request, or null of not handled.
 	 */
-	public async requestResponse(endpointDefinition: EndpointDefinition,
-		request?: EndpointFavoriteListRequest | EndpointFavoriteGetRequest): Promise<EndpointFavoriteListResponse|
-		EndpointFavoriteGetResponse|undefined> {
-		if(endpointDefinition.type === "module" && !isEmpty(request)) {
-			if("id" in request) {
+	public async requestResponse(
+		endpointDefinition: EndpointDefinition,
+		request?: EndpointFavoriteListRequest | EndpointFavoriteGetRequest
+	): Promise<EndpointFavoriteListResponse | EndpointFavoriteGetResponse | undefined> {
+		if (endpointDefinition.type === "module" && !isEmpty(request)) {
+			if ("id" in request) {
 				return this.requestResponseGetFavorite(request);
 			}
 			return this.requestResponseFavorites(request);
 		}
-		throw new Error(`This endpoint module only supports the type 'module' and being passed a request. The following endpoint type was specified: ${endpointDefinition.type} and request passed: ${!isEmpty(request)}`);
+		throw new Error(
+			`This endpoint module only supports the type 'module' and being passed a request. The following endpoint type was specified: ${
+				endpointDefinition.type
+			} and request passed: ${!isEmpty(request)}`
+		);
 	}
 
 	/**
@@ -96,13 +115,14 @@ export class FavoriteLocalStorageProvider implements Endpoint<unknown> {
 	 * @returns the list of favorites
 	 */
 	private async requestResponseFavorites(
-		request: EndpointFavoriteListRequest): Promise<EndpointFavoriteListResponse> {
-			const favorites = this.getFavorites();
-			if(isEmpty(request.favoriteType)) {
-				const filteredEntries = favorites.filter((entry) => entry.payload.type === request.favoriteType);
-				return { entries: filteredEntries };
-			}
-			return { entries: favorites };
+		request: EndpointFavoriteListRequest
+	): Promise<EndpointFavoriteListResponse> {
+		const favorites = this.getFavorites();
+		if (isEmpty(request.favoriteType)) {
+			const filteredEntries = favorites.filter((entry) => entry.payload.type === request.favoriteType);
+			return { entries: filteredEntries };
+		}
+		return { entries: favorites };
 	}
 
 	/**
@@ -111,14 +131,14 @@ export class FavoriteLocalStorageProvider implements Endpoint<unknown> {
 	 * @returns the favorite
 	 */
 	private async requestResponseGetFavorite(
-		request: EndpointFavoriteGetRequest): Promise<EndpointFavoriteGetResponse|undefined> {
-			const favorites = this.getFavorites();
-			if(!isEmpty(request.id)) {
-				const matchedEntry =
-				favorites.find((entry) => entry.payload.id === request.id);
-				return matchedEntry;
-			}
-			return undefined;
+		request: EndpointFavoriteGetRequest
+	): Promise<EndpointFavoriteGetResponse | undefined> {
+		const favorites = this.getFavorites();
+		if (!isEmpty(request.id)) {
+			const matchedEntry = favorites.find((entry) => entry.payload.id === request.id);
+			return matchedEntry;
+		}
+		return undefined;
 	}
 
 	/**
@@ -126,53 +146,49 @@ export class FavoriteLocalStorageProvider implements Endpoint<unknown> {
 	 * @param request the request for a favorite
 	 * @returns the favorite
 	 */
-	private async requestResponseSetFavorite(
-		request: EndpointFavoriteSetRequest): Promise<boolean> {
-			const favorites = this.getFavorites();
-			if(!isEmpty(request.id)) {
-				const existingEntryIndex =
-				favorites.findIndex((entry) => entry.payload.id === request.id);
-				const entryToSave = { metaData: request.metaData, payload: request.payload }; 
-				if(existingEntryIndex === -1) {
-					favorites.push(entryToSave);
-				} else {
-					favorites[existingEntryIndex] = entryToSave;
-				}
-				this.saveFavorites(favorites);
-				return true;
+	private async requestResponseSetFavorite(request: EndpointFavoriteSetRequest): Promise<boolean> {
+		const favorites = this.getFavorites();
+		if (!isEmpty(request.id)) {
+			const existingEntryIndex = favorites.findIndex((entry) => entry.payload.id === request.id);
+			const entryToSave = { metaData: request.metaData, payload: request.payload };
+			if (existingEntryIndex === -1) {
+				favorites.push(entryToSave);
+			} else {
+				favorites[existingEntryIndex] = entryToSave;
 			}
-			return false;
+			this.saveFavorites(favorites);
+			return true;
+		}
+		return false;
 	}
 
 	/**
-	 * Handles the request for the deletion of a favorite
+	 * Handles the request for the deletion of a favorite.
 	 * @param request the request for a favorite to be deleted
 	 * @returns whether or not the request was successful
 	 */
-	private async requestResponseRemoveFavorite(
-		request: EndpointFavoriteRemoveRequest): Promise<boolean> {
-			const favorites = this.getFavorites();
-			if(!isEmpty(request.id)) {
-				const existingEntryIndex =
-				favorites.findIndex((entry) => entry.payload.id === request.id);
-				if(existingEntryIndex === -1) {
-					return false;
-				}
-				delete favorites[existingEntryIndex];
-				this.saveFavorites(favorites);
-				return true;
+	private async requestResponseRemoveFavorite(request: EndpointFavoriteRemoveRequest): Promise<boolean> {
+		const favorites = this.getFavorites();
+		if (!isEmpty(request.id)) {
+			const existingEntryIndex = favorites.findIndex((entry) => entry.payload.id === request.id);
+			if (existingEntryIndex === -1) {
+				return false;
 			}
-			return false;
+			delete favorites[existingEntryIndex];
+			this.saveFavorites(favorites);
+			return true;
+		}
+		return false;
 	}
 
 	/**
-	 * Return a list of saved favorites
+	 * Return a list of saved favorites.
 	 * @returns The saved list of favorites
 	 */
 	private getFavorites(): EndpointFavoriteEntry[] {
 		const favorites = localStorage.getItem(this._favoritesKey);
 
-		if(favorites === null) {
+		if (favorites === null) {
 			return [];
 		}
 
