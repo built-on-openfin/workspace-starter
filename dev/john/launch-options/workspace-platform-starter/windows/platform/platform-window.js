7555ae7deb546bf9b4156d4c169f7d1c67960402
johnman <john.mandia@gmail.com>
2023-10-12 07:54:26 +0000

    Deploying to gh-pages from @ built-on-openfin/workspace-starter@0b7dd7e5c5eb2b5ebe916d9ce6f5c1b0897a5bd5 🚀

---
+++ dev/john/launch-options/workspace-platform-starter/windows/platform/platform-window.js
@@ -0,0 +1,58 @@
+const CONTAINER_ID = 'layout-container';
+
+const thisWindow = fin.Window.getCurrentSync();
+
+window.addEventListener('DOMContentLoaded', async () => {
+	// Initialize the window layout
+	await fin.Platform.Layout.init({ containerId: CONTAINER_ID });
+
+	// Setup the title bar elements.
+	setupTitleBar();
+});
+
+/**
+ * Maximize or restore the window.
+ * @returns Nothing.
+ */
+async function maxOrRestore() {
+	if ((await thisWindow.getState()) === 'normal') {
+		return thisWindow.maximize();
+	}
+
+	return thisWindow.restore();
+}
+
+/**
+ * Close the window.
+ * @returns Nothing.
+ */
+async function closeWindow() {
+	await thisWindow.close();
+}
+
+/**
+ * Minimize the window.
+ * @returns Nothing.
+ */
+async function minimizeWindow() {
+	await thisWindow.minimize();
+}
+
+/**
+ * Setup the title bar for the window and it's button click handlers.
+ */
+function setupTitleBar() {
+	const minBtn = document.querySelector('#minimize-button');
+	const maxBtn = document.querySelector('#expand-button');
+	const closeBtn = document.querySelector('#close-button');
+
+	if (minBtn) {
+		minBtn.addEventListener('click', minimizeWindow);
+	}
+	if (maxBtn) {
+		maxBtn.addEventListener('click', maxOrRestore);
+	}
+	if (closeBtn) {
+		closeBtn.addEventListener('click', closeWindow);
+	}
+}
