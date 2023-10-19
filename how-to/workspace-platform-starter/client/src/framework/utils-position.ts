0aa5298adf1d8a004323c2a4b2fb2dd5dc1eb81f
Martyn Janes <martyn@obany.com>
2023-10-19 15:00:11 +0200

    Popup menu improvements (#582)
    * Popup menu improvements
    
    * Update changelog

--- how-to/workspace-platform-starter/client/src/framework/utils-position.ts
+++ how-to/workspace-platform-starter/client/src/framework/utils-position.ts
@@ -5,23 +5,44 @@ import { isEmpty } from "./utils";
  * Provides x and y co-ordinates to position a window of a given size in relation to another window/view.
  * @param clientIdentity The identity of the view/window these x/y co-ordinates should be in relation to.
  * @param dimensions The dimensions of the window that will be placed in the center of the screen.
- * @param dimensions.height The height of the window that is going to be placed
  * @param dimensions.width The width of the window that is going to be placed.
+ * @param dimensions.height The height of the window that is going to be placed.
  * @returns The x, y co-ordinates to position the window
  */
-export async function getCenterPosition(
+export async function centerContentInIdentity(
 	clientIdentity: OpenFin.Identity,
-	dimensions: { height: number; width: number }
+	dimensions: { width: number; height: number }
 ): Promise<{ x: number; y: number } | undefined> {
-	const monitorInfo = await getMonitorInfo(clientIdentity);
-	const height = monitorInfo.availableRect.bottom - monitorInfo.availableRect.top;
-	const width = monitorInfo.availableRect.right - monitorInfo.availableRect.left;
+	const bounds = await getIdentityBounds(clientIdentity);
+	const boundsCenter = getBoundsCenter(bounds);
+	const monitorInfo = await findMonitorContainingPoint(boundsCenter);
+	return centerContentInRect(monitorInfo.availableRect, dimensions);
+}
+
+/**
+ * Provides x and y co-ordinates to position content of a given size in relation to a rect.
+ * @param availableRect The available rect to position the content in.
+ * @param availableRect.left The available rect left to position the content in.
+ * @param availableRect.top The available rect top to position the content in.
+ * @param availableRect.right The available rect right to position the content in.
+ * @param availableRect.bottom The available rect bottom to position the content in.
+ * @param contentDimensions The dimensions of the content that will be placed in the center of the screen.
+ * @param contentDimensions.width The width of the content that is going to be placed.
+ * @param contentDimensions.height The height of the content that is going to be placed.
+ * @returns The x, y co-ordinates to position the content
+ */
+export function centerContentInRect(
+	availableRect: { left: number; top: number; right: number; bottom: number },
+	contentDimensions: { width: number; height: number }
+): { x: number; y: number } {
+	const height = availableRect.bottom - availableRect.top;
+	const width = availableRect.right - availableRect.left;
 	const dividedRectWidth = width / 2;
 	const dividedRectHeight = height / 2;
-	const dividedDimensionWidth = dimensions.width / 2;
-	const dividedDimensionHeight = dimensions.height / 2;
-	const x = monitorInfo.availableRect.left + dividedRectWidth - dividedDimensionWidth;
-	const y = monitorInfo.availableRect.top + dividedRectHeight - dividedDimensionHeight;
+	const dividedDimensionWidth = contentDimensions.width / 2;
+	const dividedDimensionHeight = contentDimensions.height / 2;
+	const x = availableRect.left + dividedRectWidth - dividedDimensionWidth;
+	const y = availableRect.top + dividedRectHeight - dividedDimensionHeight;
 
 	return { x, y };
 }
@@ -31,7 +52,9 @@ export async function getCenterPosition(
  * @param clientIdentity The identity of the view/window to check against.
  * @returns The monitor the view/window lives on or undefined if no match was found.
  */
-export async function getMonitorInfo(clientIdentity: OpenFin.Identity): Promise<OpenFin.MonitorDetails> {
+export async function getIdentityBounds(
+	clientIdentity: OpenFin.Identity
+): Promise<OpenFin.Bounds | undefined> {
 	let bounds: OpenFin.Bounds | undefined;
 	let currentWindow: OpenFin.Window | undefined;
 
@@ -50,6 +73,7 @@ export async function getMonitorInfo(clientIdentity: OpenFin.Identity): Promise<
 			// it wasn't a window
 		}
 	}
+
 	if (isEmpty(bounds)) {
 		try {
 			bounds = await fin.me.getBounds();
@@ -58,33 +82,76 @@ export async function getMonitorInfo(clientIdentity: OpenFin.Identity): Promise<
 		}
 	}
 
-	const monitorInfo = await fin.System.getMonitorInfo();
-	if (!isEmpty(bounds)) {
-		if (isMatchingMonitor(monitorInfo.primaryMonitor.availableRect, bounds)) {
-			return monitorInfo.primaryMonitor;
-		}
+	return bounds;
+}
 
-		for (const nonPrimaryMonitor of monitorInfo.nonPrimaryMonitors) {
-			if (isMatchingMonitor(nonPrimaryMonitor.availableRect, bounds)) {
-				return nonPrimaryMonitor;
+/**
+ * Find the monitor which contains the point and returns it.
+ * @param point The point coord to locate.
+ * @param point.x The x coord
+ * @param point.y The y coord
+ * @returns The monitor containing the point.
+ */
+export async function findMonitorContainingPoint(point: {
+	x?: number;
+	y?: number;
+}): Promise<OpenFin.MonitorDetails> {
+	const monitorInfo = await fin.System.getMonitorInfo();
+
+	const x = point.x;
+	const y = point.y;
+	if (!isEmpty(x) && !isEmpty(y)) {
+		for (const monitor of monitorInfo.nonPrimaryMonitors) {
+			if (pointInRect({ x, y }, monitor.monitorRect)) {
+				return monitor;
 			}
 		}
 	}
-	// if no match is found return the primary monitor
+
 	return monitorInfo.primaryMonitor;
 }
 
 /**
- * This checks to see if the bounds of a view or window fall within a specific monitor.
- * @param rect The rectangle of a specific monitor
- * @param bounds The bounds of the window you are checking against.
- * @returns Whether or not the passed monitor rect was a match.
+ * Is the point in the rectangle.
+ * @param point The coord to match.
+ * @param point.x The x coord.
+ * @param point.y The y coord.
+ * @param rect The rect.
+ * @param rect.top The rect top.
+ * @param rect.left The rect left.
+ * @param rect.bottom The rect bottom.
+ * @param rect.right The rect right.
+ * @returns True if the point is in the rect.
  */
-function isMatchingMonitor(rect: OpenFin.RectangleByEdgePositions, bounds: OpenFin.Bounds): boolean {
-	return (
-		rect.left <= bounds.left &&
-		rect.right >= bounds.left &&
-		rect.top <= bounds.top &&
-		rect.bottom >= bounds.top
-	);
+export function pointInRect(
+	point: { x: number; y: number },
+	rect: {
+		top: number;
+		left: number;
+		bottom: number;
+		right: number;
+	}
+): boolean {
+	return point.x >= rect.left && point.x <= rect.right && point.y >= rect.top && point.y <= rect.bottom;
+}
+
+/**
+ * Get the center for a bounding rectangle.
+ * @param bounds The rect
+ * @param bounds.top The rect top
+ * @param bounds.left The rect left
+ * @param bounds.width The rect bottom
+ * @param bounds.hgith The rect right
+ * @returns True if the point is in the rect.
+ */
+export function getBoundsCenter(bounds?: OpenFin.Bounds): { x?: number; y?: number } {
+	let boundsCenterX;
+	let boundsCenterY;
+	if (!isEmpty(bounds)) {
+		const halfWidth = bounds.width / 2;
+		const halfHeght = bounds.height / 2;
+		boundsCenterX = bounds.left + halfWidth;
+		boundsCenterY = bounds.top + halfHeght;
+	}
+	return { x: boundsCenterX, y: boundsCenterY };
 }
