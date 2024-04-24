cb88a2c76b9a1c6b062136cbe4e842bcef6da797
John <john.mandia@gmail.com>
2024-04-24 19:25:37 +0100

    Bumping runtime patch version. (#712)
    Patch the runtime in manifests and tests.

--- how-to/workspace-platform-starter/e2e/tests/platform.spec.ts
+++ how-to/workspace-platform-starter/e2e/tests/platform.spec.ts
@@ -16,7 +16,7 @@ describe("Platform", () => {
 	it("The runtime version should be set", async () => {
 		const fin = await OpenFinProxy.fin();
 		const version = await fin.System.getVersion();
-		expect(version).toEqual("36.122.80.10");
+		expect(version).toEqual("36.122.80.11");
 	});
 
 	it("Can open the home window", async () => {
