c20f5b6f1ff75fc6abef4a59e066e78b85de1604
johnman <john.mandia@gmail.com>
2024-05-29 12:20:23 +0000

    Deploying to gh-pages from @ built-on-openfin/workspace-starter@989b18dfc3961304bb3229a84774b92ea9ad2aa5 🚀

--- main/workspace-platform-starter/common/views/contact/participant-selection/index.js
+++ main/workspace-platform-starter/common/views/contact/participant-selection/index.js
@@ -6,83 +6,92 @@ window.addEventListener('DOMContentLoaded', initializeDOM);
  * Initialize the DOM.
  */
 async function initializeDOM() {
-	if (window.fdc3) {
-		await usersModule.initialize();
+	if (window.fdc3 !== undefined) {
+		setupListeners();
+	} else {
+		window.addEventListener('fdc3Ready', async () => {
+			setupListeners();
+		});
+	}
+}
 
-		let hasViewContactIntentHandler = false;
-		try {
-			const info = await fin.me.interop.getInfoForIntent({
-				name: 'ViewContact'
-			});
-			if (info) {
-				hasViewContactIntentHandler = true;
-			}
-		} catch {}
+/**
+ * Sets up the related fdc3 listeners once fdc3 is available.
+ */
+async function setupListeners() {
+	await usersModule.initialize();
 
-		if (!hasViewContactIntentHandler) {
-			console.warn('No ViewContact intent handler is available, will not show Raise Intent button');
+	let hasViewContactIntentHandler = false;
+	try {
+		const info = await fdc3.findIntent('ViewContact');
+		if (info) {
+			hasViewContactIntentHandler = true;
+		}
+	} catch {}
+
+	if (!hasViewContactIntentHandler) {
+		console.warn('No ViewContact intent handler is available, will not show Raise Intent button');
+	}
+
+	const users = usersModule.getUsers();
+
+	const tbody = document.querySelector('tbody');
+
+	for (const user of users) {
+		const image = document.createElement('img');
+		image.src = usersModule.getProfilePic(user);
+		image.width = 28;
+		image.height = 28;
+		image.classList.add('profile-pic');
+		image.style.borderRadius = '5px';
+
+		const imageCell = document.createElement('td');
+		imageCell.append(image);
+
+		const dateCell = document.createElement('td');
+		dateCell.textContent = user.interactionDate;
+
+		const nameCell = document.createElement('td');
+		nameCell.textContent = user.name;
+
+		const emailCell = document.createElement('td');
+		emailCell.textContent = user.email;
+
+		const typeCell = document.createElement('td');
+		typeCell.textContent = user.interactionType;
+
+		const actionRow = document.createElement('div');
+		actionRow.classList.add('row');
+		actionRow.classList.add('gap10');
+
+		const actionCell = document.createElement('td');
+		actionCell.append(actionRow);
+
+		const row = document.createElement('tr');
+		row.append(imageCell);
+		row.append(dateCell);
+		row.append(nameCell);
+		row.append(emailCell);
+		row.append(typeCell);
+		row.append(actionCell);
+
+		const selectButton = document.createElement('button');
+		selectButton.textContent = 'Select';
+		selectButton.classList.add('small');
+		selectButton.addEventListener('click', () => selectParticipant(user));
+
+		actionRow.append(selectButton);
+
+		if (hasViewContactIntentHandler) {
+			const raiseIntentButton = document.createElement('button');
+			raiseIntentButton.textContent = 'Raise Intent';
+			raiseIntentButton.classList.add('small');
+			raiseIntentButton.classList.add('secondary');
+			raiseIntentButton.addEventListener('click', () => raiseIntent(user));
+			actionRow.append(raiseIntentButton);
 		}
 
-		const users = usersModule.getUsers();
-
-		const tbody = document.querySelector('tbody');
-
-		for (const user of users) {
-			const image = document.createElement('img');
-			image.src = usersModule.getProfilePic(user);
-			image.width = 28;
-			image.height = 28;
-			image.classList.add('profile-pic');
-			image.style.borderRadius = '5px';
-
-			const imageCell = document.createElement('td');
-			imageCell.append(image);
-
-			const dateCell = document.createElement('td');
-			dateCell.textContent = user.interactionDate;
-
-			const nameCell = document.createElement('td');
-			nameCell.textContent = user.name;
-
-			const emailCell = document.createElement('td');
-			emailCell.textContent = user.email;
-
-			const typeCell = document.createElement('td');
-			typeCell.textContent = user.interactionType;
-
-			const actionRow = document.createElement('div');
-			actionRow.classList.add('row');
-			actionRow.classList.add('gap10');
-
-			const actionCell = document.createElement('td');
-			actionCell.append(actionRow);
-
-			const row = document.createElement('tr');
-			row.append(imageCell);
-			row.append(dateCell);
-			row.append(nameCell);
-			row.append(emailCell);
-			row.append(typeCell);
-			row.append(actionCell);
-
-			const selectButton = document.createElement('button');
-			selectButton.textContent = 'Select';
-			selectButton.classList.add('small');
-			selectButton.addEventListener('click', () => selectParticipant(user));
-
-			actionRow.append(selectButton);
-
-			if (hasViewContactIntentHandler) {
-				const raiseIntentButton = document.createElement('button');
-				raiseIntentButton.textContent = 'Raise Intent';
-				raiseIntentButton.classList.add('small');
-				raiseIntentButton.classList.add('secondary');
-				raiseIntentButton.addEventListener('click', () => raiseIntent(user));
-				actionRow.append(raiseIntentButton);
-			}
-
-			tbody.append(row);
-		}
+		tbody.append(row);
 	}
 }
 
