d85b278e9503d5a84453c42b2f9c022b961ccf6f
johnman <john.mandia@gmail.com>
2024-11-13 13:47:05 +0000

    Deploying to gh-pages from @ built-on-openfin/workspace-starter@873041418c82849cd248c8aa7deb0687009f11bb 🚀

--- main/integrate-with-excel/js/excel.bundle.js
+++ main/integrate-with-excel/js/excel.bundle.js
@@ -313,7 +313,7 @@ var e,t,r={d:(e,t)=>{for(var a in t)r.o(t,a)&&!r.o(e,a)&&Object.defineProperty(e
 /******/ 
 /************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+// This entry needs to be wrapped in an IIFE because it needs to be isolated against other modules in the chunk.
 (() => {
 /*!************************************!*\
   !*** ./client/src/excel-window.ts ***!
