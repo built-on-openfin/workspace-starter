fdb8700b19abc21f5f091867fa952c49c6ee6520
John <john.mandia@gmail.com>
2025-06-25 17:55:13 +0100

    workspace v21 patch (#778)
    * Update to latest patched version of workspace v21
    
    * Upgrade to stable version.

--- how-to/workspace-platform-starter/templates/src/integrations/integrations.ts
+++ how-to/workspace-platform-starter/templates/src/integrations/integrations.ts
@@ -352,7 +352,7 @@ export class ExampleIntegrationsProvider implements IntegrationModule<ExampleInt
 					imageSrcId:
 						"https://built-on-openfin.github.io/workspace-starter/workspace/v21.0.0/workspace-platform-starter/common/images/ms/apps/outlook-calendar.svg",
 					textContent: "This is text content",
-					imageContent: "https://cdn.openfin.co/workspace/21.0.13/icons/defaultFavicon.svg",
+					imageContent: "https://cdn.openfin.co/workspace/21.0.16/icons/defaultFavicon.svg",
 					listContent: [
 						["Label 1", "Value 1"],
 						["Label 2", "Value 2"]
