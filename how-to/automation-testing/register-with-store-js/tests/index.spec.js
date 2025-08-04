8091f25bba0c21cbc54ba3c86f77e58f2bad1245
Elliott Burr <elliott.burr@openfin.co>
2025-08-04 12:35:07 -0400

    Bump workspace to 22.3.9 / Runtime to 42.100.101

--- how-to/automation-testing/register-with-store-js/tests/index.spec.js
+++ how-to/automation-testing/register-with-store-js/tests/index.spec.js
@@ -1,25 +1,25 @@
-const { OpenFinProxy, OpenFinSystem, OpenFinStore, WebDriver } = require('@openfin/automation-helpers');
-const { expect } = require('chai');
+const { OpenFinProxy, OpenFinSystem, OpenFinStore, WebDriver } = require("@openfin/automation-helpers");
+const { expect } = require("chai");
 
-describe('Register with Store', () => {
-	it('The runtime is ready', async () => {
-		const isReady = await OpenFinSystem.waitForReady(10000);
-		expect(isReady).to.equal(true);
-	});
+describe("Register with Store", () => {
+  it("The runtime is ready", async () => {
+    const isReady = await OpenFinSystem.waitForReady(10000);
+    expect(isReady).to.equal(true);
+  });
 
-	it('The title should be set', async () => {
-		const title = await WebDriver.getTitle();
-		expect(title).to.equal('Platform Provider');
-	});
+  it("The title should be set", async () => {
+    const title = await WebDriver.getTitle();
+    expect(title).to.equal("Platform Provider");
+  });
 
-	it('The runtime version should be set', async () => {
-		const fin = await OpenFinProxy.fin();
-		const version = await fin.System.getVersion();
-		expect(version).to.equal('42.138.100.97');
-	});
+  it("The runtime version should be set", async () => {
+    const fin = await OpenFinProxy.fin();
+    const version = await fin.System.getVersion();
+    expect(version).to.equal("42.138.100.101");
+  });
 
-	it('Can open the store window', async () => {
-		const isShown = await OpenFinStore.show(60000);
-		expect(isShown).to.equal(true);
-	});
+  it("Can open the store window", async () => {
+    const isShown = await OpenFinStore.show(60000);
+    expect(isShown).to.equal(true);
+  });
 });
