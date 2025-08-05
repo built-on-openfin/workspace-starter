2bbeb2d675aa1c6fa2ab6d11f6bd2f41b4b09d8b
Nick Newman <nicholas.newman90@gmail.com>
2025-08-05 11:17:05 -0400

    Add basic Dock3 workspace starter how-to example (#780)
    Add basic Dock3 workspace starter how-to example
    
    Co-authored-by: Nick Newman <nick.newman@here.io>

---
+++ how-to/register-with-dock3-basic/client/src/dock-providers.ts
@@ -0,0 +1,53 @@
+import type { Dock3Config } from "@openfin/workspace-platform";
+import { defaultContentMenu, defaultDockFavorites } from "./dock-entries";
+
+const sampleProviders = {
+	google: {
+		id: "google",
+		title: "Google",
+		icon: {
+			light: "https://www.yahoo.com/favicon.ico",
+			dark: "https://www.google.com/favicon.ico"
+		},
+		favorites: defaultDockFavorites,
+		contentMenu: defaultContentMenu
+	},
+	barclays: {
+		id: "barclays",
+		title: "Barclays",
+		icon: {
+			light: "https://www.barclays.co.uk/favicon.ico",
+			dark: "https://www.barclays.co.uk/favicon.ico"
+		},
+		favorites: defaultDockFavorites,
+		contentMenu: defaultContentMenu
+	},
+	arcore: {
+		id: "arcore",
+		title: "ARCore",
+		icon: {
+			light: "https://www.arcore.com/favicon.ico",
+			dark: "https://www.arcore.com/favicon.ico"
+		},
+		favorites: defaultDockFavorites,
+		contentMenu: defaultContentMenu
+	},
+	nameless: {
+		id: "nameless",
+		title: "",
+		icon: "",
+		favorites: [],
+		contentMenu: []
+	}
+};
+
+/**
+ * Return specified dock3 config object.
+ * @param set id of requested config
+ * @returns requested conifg based on param id
+ */
+function getDock3Provider(set: keyof typeof sampleProviders): Dock3Config {
+	return sampleProviders[set] as Dock3Config;
+}
+
+export default getDock3Provider;
