716c2c55367f2f38ce78152136ab3424e57e7304
John <john.mandia@gmail.com>
2026-04-30 13:33:47 +0100

    Dev/john/workspace 23 update (#828)
    * Upgrade to stable release
    
    * Updated the schema

--- how-to/automation-testing/register-with-store-js/tests/index.spec.js
+++ how-to/automation-testing/register-with-store-js/tests/index.spec.js
@@ -15,7 +15,7 @@ describe("Register with Store", () => {
   it("The runtime version should be set", async () => {
     const fin = await OpenFinProxy.fin();
     const version = await fin.System.getVersion();
-    expect(version).to.equal("43.142.101.2");
+    expect(version).to.equal("43.142.102.2");
   });
 
   it("Can open the store window", async () => {
--- how-to/migrate-from-a-previous-version/README.md
+++ how-to/migrate-from-a-previous-version/README.md
@@ -5,7 +5,7 @@
 
 ## Migrate from a previous version - From v19.1 to v19.2
 
-- [Please see the v19.2 Release Notes](https://developer.openfin.co/versions/?product=Workspace#23.0.20)
+- [Please see the v19.2 Release Notes](https://developer.openfin.co/versions/?product=Workspace#23.0.21)
 
 ## Migrate from a previous version - From v19 to v19.1
 
